9702361ed1439a83a71409aa8d2bfc6b69fb7036
Andreas Skomedal <andreas.skomedal@nav.no>
2020-02-28 16:36:04 +0100

    backend layered jar

---
+++ apps/backend/src/main/java/no/nav/data/team/TeamCatalogApplication.java
@@ -0,0 +1,13 @@
+package no.nav.data.team;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamCatalogApplication.class, args);
+	}
+
+}
---
+++ apps/backend/src/test/java/no/nav/data/team/TeamCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package no.nav.data.team;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

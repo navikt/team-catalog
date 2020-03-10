03ba67fbe512a98eff5c5ef9ed092bda1d27ba88
danishnav <54104284+danishnav@users.noreply.github.com>
2020-03-10 15:21:29 +0100

    chenged config for teamcatalog frontend

---
+++ apps/frontend/src/setupProxy.js
@@ -0,0 +1,31 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+// Used in local development server only
+module.exports = function (app) {
+
+  const target = 'http://localhost:8080'
+  const headers = {
+    'Nav-Consumer-Id': 'teamsfrontend-local'
+  }
+
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      pathRewrite: {
+        '^/api': ''
+      },
+      target,
+      headers
+    })
+  );
+
+  app.use(
+    '/login',
+    createProxyMiddleware({ target, headers })
+  );
+  app.use(
+    '/logout',
+    createProxyMiddleware({ target, headers })
+  );
+
+};

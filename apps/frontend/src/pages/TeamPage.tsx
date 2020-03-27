0edbfc41f4942177ae2b781b2863acc0e5c5fb53
Shayan Yazdanmehr <shayan.yazdanmehr@nav.no>
2020-03-27 10:20:47 +0100

    Adding product area name to the team view

--- apps/frontend/src/pages/TeamPage.tsx
+++ apps/frontend/src/pages/TeamPage.tsx
@@ -1,56 +1,57 @@
 import * as React from 'react'
 import Metadata from '../components/common/Metadata'
-import { RouteComponentProps } from 'react-router-dom'
-import { ProductTeam, ProductArea } from '../constants'
-import { getTeam } from '../api/teamApi'
-import { H4, Label2 } from 'baseui/typography'
-import { Block } from 'baseui/block'
-import MemberList from '../components/Team/MemberList'
+import {RouteComponentProps} from 'react-router-dom'
+import {ProductTeam} from '../constants'
+import {getTeam} from '../api/teamApi'
+import {H4, Label2} from 'baseui/typography'
+import {Block} from 'baseui/block'
 import AccordionMembers from '../components/Team/AccordionMembers'
-import { theme } from '../util'
+import {theme} from '../util'
+import {getProductArea} from "../api";
 
 export type PathParams = { id: string }
 
 const TeamPage = (props: RouteComponentProps<PathParams>) => {
-    const [loading, setLoading] = React.useState<boolean>(false)
-    const [team, setTeam] = React.useState<ProductTeam>()
-    const [productAreaName, setProductAreaName] = React.useState<string>('')
+  const [loading, setLoading] = React.useState<boolean>(false)
+  const [team, setTeam] = React.useState<ProductTeam>()
+  const [productAreaName, setProductAreaName] = React.useState<string>('')
 
-    React.useEffect(() => {
-        (async () => {
-            if (props.match.params.id) {
-                setLoading(true)
-                const res = await getTeam(props.match.params.id)
-                console.log(res)
-                setTeam(res)
-                setLoading(false)
-            }
-        })()
+  React.useEffect(() => {
+    (async () => {
+      if (props.match.params.id) {
+        setLoading(true)
+        const teamResponse = await getTeam(props.match.params.id)
+        const productAreaResponse = await getProductArea(teamResponse.productAreaId)
+        setProductAreaName(productAreaResponse.name)
+        setTeam(teamResponse)
+        setLoading(false)
+      }
+    })()
 
-    }, [props.match.params])
+  }, [props.match.params])
 
-    return (
+  return (
+    <>
+      {!loading && team && (
         <>
-            {!loading && team && (
-                <>
-                    <H4>{team.name}</H4>
-                    <Block>
-                        <Metadata
-                            productAreaName="Navn"
-                            description={team.description}
-                            slackChannel={team.slackChannel}
-                            naisTeams={team.naisTeams}
-                        />
-                    </Block>
-                    <Block width="50%" marginTop="2rem">
-                        <Label2 marginBottom={theme.sizing.scale600}>Medlemmer av teamet</Label2>
-                        <AccordionMembers members={team.members} />
-                    </Block>
+          <H4>{team.name}</H4>
+          <Block>
+            <Metadata
+              productAreaName={productAreaName}
+              description={team.description}
+              slackChannel={team.slackChannel}
+              naisTeams={team.naisTeams}
+            />
+          </Block>
+          <Block width="50%" marginTop="2rem">
+            <Label2 marginBottom={theme.sizing.scale600}>Medlemmer av teamet</Label2>
+            <AccordionMembers members={team.members}/>
+          </Block>
 
-                </>
-            )}
         </>
-    )
+      )}
+    </>
+  )
 }
 
 export default TeamPage

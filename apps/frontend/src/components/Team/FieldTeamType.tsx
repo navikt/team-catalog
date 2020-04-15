c065a9a75ec01a372c9d7c799614c0aa218f09d1
Shayan Yazdanmehr <shayan.yazdanmehr@nav.no>
2020-04-15 13:53:50 +0200

    Add norwegian text for team types

--- apps/frontend/src/components/Team/FieldTeamType.tsx
+++ apps/frontend/src/components/Team/FieldTeamType.tsx
@@ -3,21 +3,21 @@ import * as React from "react";
 import {Field, FieldProps} from "formik";
 import {ProductTeamFormValues, TeamType} from "../../constants";
 import {Block} from "baseui/block";
+import {intl} from "../../util/intl/intl";
 
 const FieldTeamType = (props: {teamType: TeamType}) => {
   const {teamType} = props
-  const [value, setValue] = React.useState<Value>(teamType?[{id:teamType,label:teamType}]:[])
+  const [value, setValue] = React.useState<Value>(teamType?[{id:teamType,label:intl[teamType]}]:[])
   return (
     <Field
       name='teamType'
       render={({form}: FieldProps<ProductTeamFormValues>) => (
         <Block marginRight='10px' width='100%'>
           <Select
-            options={Object.values(TeamType).map(value1 => ({id: value1, label: value1}))}
+            options={Object.values(TeamType).map(tt => ({id: tt, label: intl[tt]}))}
             onChange={({value}) => {
               setValue(value)
               form.setFieldValue('teamType', value.length > 0 ? value[0].id : '')
-              console.log(value)
             }}
             value={value}
             placeholder='Velg en teamtype'

4dc69aadc937baef508d79348ae9c04964f1e7e3
Andreas Skomedal <andreas.skomedal@nav.no>
2020-04-21 10:14:00 +0200

    Update profile pictures

--- apps/frontend/src/components/common/UserImage.tsx
+++ apps/frontend/src/components/common/UserImage.tsx
@@ -1,18 +1,24 @@
 import User from '../../resources/user.svg'
 import * as React from 'react'
+import { StatefulTooltip } from 'baseui/tooltip'
 
 const getResourceImage = (navIdent: string) => `https://teamkatalog-api.nais.adeo.no/resource/${navIdent}/photo`
 
 
 export const UserImage = (props: { ident: string, maxWidth: string }) => {
-  const [image, setImage] = React.useState(getResourceImage(props.ident))
+    const [image, setImage] = React.useState(getResourceImage(props.ident))
 
-  return (
-    <img
-      src={image}
-      onError={() => setImage(User)}
-      alt="Member image"
-      style={{maxWidth: props.maxWidth}}
-    />
-  )
+    return (
+        <StatefulTooltip
+            content={"Trykk for oppdatering av bilde"}
+        >
+            <img
+                src={image}
+                onError={() => setImage(User)}
+                onClick={() => setImage(getResourceImage(props.ident) + "?forceUpdate=true")}
+                alt={`Member image${props.ident}`}
+                style={{maxWidth: props.maxWidth}}
+            />
+        </StatefulTooltip>
+    )
 }

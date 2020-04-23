d280e781650ffaaf1fafe2422443ae6753beaee0
Andreas Skomedal <andreas.skomedal@nav.no>
2020-04-23 09:18:57 +0200

    unique session cookie

--- apps/backend/src/main/java/no/nav/data/team/common/security/SecurityConstants.java
+++ apps/backend/src/main/java/no/nav/data/team/common/security/SecurityConstants.java
@@ -8,7 +8,7 @@ import java.util.Set;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class SecurityConstants {
 
-    public static final String COOKIE_NAME = "session";
+    public static final String COOKIE_NAME = "teamsession";
     // UUID hex without dashes
     public static final int SESS_ID_LEN = 32;
     public static final String TOKEN_TYPE = "Bearer ";

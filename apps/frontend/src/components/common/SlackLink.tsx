d7b092d4d751a3d7809b2b97cee5f3492bc875e6
Andreas Skomedal <andreas.skomedal@nav.no>
2020-04-24 15:21:57 +0200

    slack new tab

--- apps/frontend/src/components/common/SlackLink.tsx
+++ apps/frontend/src/components/common/SlackLink.tsx
@@ -6,7 +6,8 @@ const slackTeamId = `T5LNAMWNA`
 export const SlackLink = (props: { channel: string }) => {
   const channel = props.channel.indexOf('#') === 0 ? props.channel.substr(1) : props.channel
   return (
-    <StyledLink href={`https://slack.com/app_redirect?team=${slackTeamId}&channel=${channel}`}>
+    <StyledLink href={`https://slack.com/app_redirect?team=${slackTeamId}&channel=${channel}`}
+                target="_blank" rel="noopener noreferrer">
       {props.channel}
     </StyledLink>
   )

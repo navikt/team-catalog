19ca8df2016a07bc6a9899748dc720e76a6eda72
Shayan Yazdanmehr <shayan.yazdanmehr@nav.no>
2020-04-24 14:39:23 +0200

    Use cards instead of table

--- apps/frontend/src/pages/ResourceView.tsx
+++ apps/frontend/src/pages/ResourceView.tsx
@@ -2,31 +2,21 @@ import React, {useEffect, useState} from "react";
 import {RouteComponentProps} from "react-router-dom";
 import {PathParams} from "./TeamPage";
 import {getResourceById} from "../api/resourceApi";
-import {ProductTeam, productTeamSort, Resource} from "../constants";
+import {ProductTeam, Resource} from "../constants";
 import {Spinner} from "baseui/spinner";
-import {Label1} from "baseui/typography";
+import {H4, Label1, Paragraph2} from "baseui/typography";
 import {Block} from "baseui/block";
 import {theme} from "../util";
 import {TextWithLabel} from "../components/common/TextWithLabel";
 import {UserImage} from "../components/common/UserImage";
 import {getAllTeamsByMemberId} from "../api/teamApi";
-import {Cell, HeadCell, Row, Table} from "../components/common/Table";
-import {useTable} from "../util/hooks";
-import RouteLink from "../components/common/RouteLink";
-
-const getTeamsByResources = (resources:Resource[])=>{
-  const promises = resources.map(
-    async (resource) => await getAllTeamsByMemberId(resource.navIdent)
-  )
-  return Promise.all(promises)
-}
+import ListTeams from "../components/ProductArea/ListTeams";
 
 const ResourceView = (props: RouteComponentProps<PathParams>) => {
 
   const [resource, setResource] = useState<Resource>()
   const [teams, setTeams] = useState<ProductTeam[]>([])
   const [isLoading, setLoading] = useState<boolean>(false)
-  const [productTeamsTable, sortColumn] = useTable<ProductTeam, keyof ProductTeam>(teams, {sorting: productTeamSort, initialSortColumn: 'name'})
 
   useEffect(() => {
     (async () => {
@@ -49,7 +39,7 @@ const ResourceView = (props: RouteComponentProps<PathParams>) => {
   return !isLoading ?
     (<>
       <Block display={"flex"} width={"100%"}>
-        <Label1>{resource?.fullName}</Label1>
+        <H4>{resource?.fullName}</H4>
       </Block>
         <Block display="flex" width='100%'>
           <Block width="30%">
@@ -66,26 +56,9 @@ const ResourceView = (props: RouteComponentProps<PathParams>) => {
             <TextWithLabel label={""} text={resource?.navIdent && (<UserImage ident={resource.navIdent} maxWidth={"100px"}/>)}/>
           </Block>
         </Block>
-      <Block width={"100%"}>
-        {teams.length>0 && (
-          <Table emptyText={"Ingen"} headers={
-            <>
-              <HeadCell title={"name"} column={"name"} tableState={[productTeamsTable, sortColumn]}/>
-              <HeadCell title={"slackChannel"} column={"slackChannel"} tableState={[productTeamsTable, sortColumn]}/>
-            </>
-          }>
-            {productTeamsTable.data.map((row, index) => (
-              <Row key={index}>
-                <Cell>
-                  <RouteLink href={`/team/${row.id}`}>{row.name}</RouteLink>
-                </Cell>
-                <Cell>
-                  {row.slackChannel}
-                </Cell>
-              </Row>
-            ))}
-          </Table>
-        )}
+      <Block marginTop="3rem">
+        <Label1 marginBottom={theme.sizing.scale800}>Teams</Label1>
+        {teams.length > 0 ? <ListTeams teams={teams} /> : <Paragraph2>Ingen teams</Paragraph2>}
       </Block>
     </>) :
     (<>

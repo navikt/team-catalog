19bd14925a9926a29cb40296c86cba12578d5ff5
Shayan Yazdanmehr <shayan.yazdanmehr@nav.no>
2020-05-06 14:53:05 +0200

    - Changing the background color for cards which contain unknown users. - Increase metadata columns in team view

--- apps/frontend/src/components/Team/ListMembers/CardMember.tsx
+++ apps/frontend/src/components/Team/ListMembers/CardMember.tsx
@@ -1,16 +1,16 @@
 import * as React from 'react'
-import { ReactNode } from 'react'
-import { Card, StyledBody } from 'baseui/card';
-import { Member, ResourceType } from '../../../constants';
-import { Label2, Paragraph2 } from 'baseui/typography';
-import { Block, BlockProps } from 'baseui/block';
-import { theme } from '../../../util';
-import { UserImage } from '../../common/UserImage'
-import { intl } from '../../../util/intl/intl'
-import { StatefulTooltip } from 'baseui/tooltip'
-import { marginAll } from '../../Style'
+import {ReactNode} from 'react'
+import {Card, StyledBody} from 'baseui/card';
+import {Member, ResourceType} from '../../../constants';
+import {Label2, Paragraph2} from 'baseui/typography';
+import {Block, BlockProps} from 'baseui/block';
+import {theme} from '../../../util';
+import {UserImage} from '../../common/UserImage'
+import {intl} from '../../../util/intl/intl'
+import {StatefulTooltip} from 'baseui/tooltip'
+import {marginAll} from '../../Style'
 import RouteLink from '../../common/RouteLink'
-import { cardShadow } from '../../common/Style'
+import {cardShadow} from '../../common/Style'
 import moment from 'moment'
 
 const contentBlockProps: BlockProps = {
@@ -32,30 +32,45 @@ type CardMemberProps = {
 }
 
 const CardMember = (props: CardMemberProps) => {
-  const { member } = props
+  const {member} = props
   return (
-    <Card title={
-      <StatefulTooltip content={`Nav-Ident: ${member.navIdent}`}>
-        <div>
-          <RouteLink href={`/resource/${member.navIdent}`} hideUnderline>
-            {member.name} {member.resourceType === ResourceType.EXTERNAL ? `(${intl.EXTERNAL})` : ''} {member.endDate && moment(member.endDate).isBefore(moment()) && '(Inaktiv)'}
-          </RouteLink>
-        </div>
-      </StatefulTooltip>
-    } overrides={{ Root: { style: { ...cardShadow.Root.style, width: '450px', ...marginAll(theme.sizing.scale200) } } }}>
+    <Card
+      title={member.name ? <StatefulTooltip content={`Nav-Ident: ${member.navIdent}`}>
+          <div>
+            <RouteLink href={`/resource/${member.navIdent}`} hideUnderline>
+              {member.name} {member.resourceType === ResourceType.EXTERNAL ? `(${intl.EXTERNAL})` : ''} {member.endDate && moment(member.endDate).isBefore(moment()) && '(Inaktiv)'}
+            </RouteLink>
+          </div>
+        </StatefulTooltip> :
+        <Block>
+          Personen er ikke funnet i NOM ({member.navIdent})
+        </Block>
+      }
+      overrides={{
+        Root:
+          {
+            style:
+              {
+                ...cardShadow.Root.style,
+                width: '450px',
+                ...marginAll(theme.sizing.scale200),
+                backgroundColor: member.name?"":"#FBEFEE"
+              }
+          }
+      }}>
 
       <StyledBody>
         <Block  {...contentBlockProps}>
           <Block flex={1}>
-            <TextWithLabel label="Roller" text={member.roles.map(r => intl[r]).join(", ")} />
-            {member.description && <TextWithLabel label="Annet" text={member.description} />}
+            <TextWithLabel label="Roller" text={member.roles.map(r => intl[r]).join(", ")}/>
+            {member.description && <TextWithLabel label="Annet" text={member.description}/>}
           </Block>
 
           <Block flex='0 0 100px'>
-            <UserImage ident={member.navIdent} maxWidth='100px' />
+            <UserImage ident={member.navIdent} maxWidth='100px'/>
           </Block>
         </Block>
-        <TextWithLabel label="Epost" text={member.email ? member.email : 'Ikke registrert'} />
+        <TextWithLabel label="Epost" text={member.email ? member.email : 'Ikke registrert'}/>
       </StyledBody>
     </Card>
   );

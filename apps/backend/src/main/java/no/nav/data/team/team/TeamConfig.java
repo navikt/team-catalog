b1946dd9524cb7b706dc24a614f2c0c580d9505c
Andreas Skomedal <andreas.skomedal@nav.no>
2020-05-08 12:32:21 +0200

    Migrate case_handler role

---
+++ apps/backend/src/main/java/no/nav/data/team/team/TeamConfig.java
@@ -0,0 +1,31 @@
+package no.nav.data.team.team;
+
+import no.nav.data.team.common.storage.StorageService;
+import no.nav.data.team.team.domain.Team;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Collections;
+
+import static no.nav.data.team.common.utils.MdcUtils.wrapAsync;
+import static no.nav.data.team.team.domain.TeamRole.CASE_HANDLER;
+import static no.nav.data.team.team.domain.TeamRole.DOMAIN_RESOURCE;
+
+@Configuration
+public class TeamConfig {
+
+    @Bean
+    public ApplicationRunner migrate(StorageService storage) {
+        return (args) -> wrapAsync(
+                () -> {
+                    var teams = storage.getAll(Team.class);
+                    teams.forEach(t -> {
+                        if (t.getMembers().stream().anyMatch(m -> Collections.replaceAll(m.getRoles(), CASE_HANDLER, DOMAIN_RESOURCE))) {
+                            storage.save(t);
+                        }
+                    });
+                }, "Database migration")
+                .run();
+    }
+}

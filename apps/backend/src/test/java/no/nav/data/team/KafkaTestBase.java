098abd52c819510d4333b5d876170602c0ec5563
Andreas Skomedal <andreas.skomedal@nav.no>
2020-05-14 13:06:37 +0200

    Use external kafka test

--- apps/backend/src/test/java/no/nav/data/team/KafkaTestBase.java
+++ apps/backend/src/test/java/no/nav/data/team/KafkaTestBase.java
@@ -2,39 +2,42 @@ package no.nav.data.team;
 
 import io.confluent.kafka.serializers.KafkaAvroDeserializer;
 import lombok.extern.slf4j.Slf4j;
+import no.nav.data.team.kafka.KafkaContainer;
+import no.nav.data.team.kafka.SchemaRegistryContainer;
 import no.nav.data.team.team.TeamUpdateProducer;
 import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.Deserializer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.test.utils.KafkaTestUtils;
 
-import java.time.Duration;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import static org.assertj.core.api.Assertions.fail;
-import static org.awaitility.Awaitility.await;
-
 @Slf4j
 public class KafkaTestBase extends IntegrationTestBase {
+
+    private static final String CONFLUENT_VERSION = "5.3.1";
+
+    private static final KafkaContainer kafkaContainer = new KafkaContainer(CONFLUENT_VERSION);
+    private static final SchemaRegistryContainer schemaRegistryContainer = new SchemaRegistryContainer(CONFLUENT_VERSION, kafkaContainer);
+
+    static {
+        // The limited junit5 support for testcontainers do not support containers to live across separate ITests
+        kafkaContainer.start();
+        schemaRegistryContainer.start();
+    }
 
     @Autowired
     protected TeamUpdateProducer teamUpdateProducer;
     @Autowired
     protected KafkaTemplate<String, String> stringTemplate;
-
-    @BeforeAll
-    static void beforeAll() {
-        kafkaEnvironment.start();
-    }
 
     @BeforeEach
     void setUpKafka() {
@@ -46,11 +49,11 @@ public class KafkaTestBase extends IntegrationTestBase {
         teamUpdateProducer.setDisable(true);
     }
 
-    protected static  <T> Consumer<String, T> createConsumer(String topic) {
+    protected static <T> Consumer<String, T> createConsumer(String topic) {
         String groupId = "teamcat-itest-" + topic;
-        Map<String, Object> configs = new HashMap<>(KafkaTestUtils.consumerProps(kafkaEnvironment.getBrokersURL(), groupId, "false"));
+        Map<String, Object> configs = new HashMap<>(KafkaTestUtils.consumerProps(KafkaContainer.getAddress(), groupId, "false"));
         configs.put("specific.avro.reader", "true");
-        configs.put("schema.registry.url", kafkaEnvironment.getSchemaRegistry().getUrl());
+        configs.put("schema.registry.url", SchemaRegistryContainer.getAddress());
         configs.put(ConsumerConfig.CLIENT_ID_CONFIG, "teamcatbacktest");
         configs.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         configs.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, KafkaAvroDeserializer.class);
@@ -60,19 +63,4 @@ public class KafkaTestBase extends IntegrationTestBase {
         return consumer;
     }
 
-    protected void awaitProducerTimeout() {
-        long start = System.currentTimeMillis();
-        await()
-                .pollDelay(Duration.ofSeconds(1))
-                .atMost(Duration.ofSeconds(10)).until(() -> {
-            try {
-                stringTemplate.send("sometopic", "somedata").get();
-                fail("Should time out and throw");
-            } catch (Exception e) {
-                log.info("producer timed out");
-            }
-            return true;
-        });
-        log.info("waited for producer to time out for {} millis", System.currentTimeMillis() - start);
-    }
 }
--- apps/backend/src/test/java/no/nav/data/team/team/TeamUpdateIT.java
+++ apps/backend/src/test/java/no/nav/data/team/team/TeamUpdateIT.java
@@ -1,6 +1,5 @@
 package no.nav.data.team.team;
 
-import no.nav.common.KafkaEnvironment.BrokerStatus;
 import no.nav.data.team.KafkaTestBase;
 import no.nav.data.team.avro.TeamUpdate;
 import no.nav.data.team.team.domain.Team;
@@ -9,21 +8,16 @@ import no.nav.data.team.team.dto.TeamMemberRequest;
 import no.nav.data.team.team.dto.TeamRequest;
 import org.apache.kafka.clients.consumer.Consumer;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.kafka.test.utils.KafkaTestUtils;
 
-import java.time.Duration;
-import java.time.LocalDateTime;
 import java.util.List;
-import java.util.UUID;
 
 import static no.nav.data.team.TestDataHelper.createNavIdent;
 import static no.nav.data.team.TestDataHelper.createResource;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.awaitility.Awaitility.await;
 
 public class TeamUpdateIT extends KafkaTestBase {
 
@@ -65,28 +59,5 @@ public class TeamUpdateIT extends KafkaTestBase {
         assertThat(update.getMembers().get(0).getName()).isEqualTo("Giv Fam");
         assertThat(update.getMembers().get(0).getRole()).isEqualTo("DEVELOPER");
         assertThat(storageService.get(savedTeam.getId(), Team.class).isUpdateSent()).isTrue();
-    }
-
-    @Disabled
-    @Test
-    void handleKafkaDown() {
-        kafkaEnvironment.getBrokers().get(0).stop();
-        team.setName("down test");
-        var savedTeam = teamService.save(team);
-        awaitProducerTimeout();
-        UUID id = savedTeam.getId();
-        assertThat(storageService.get(id, Team.class).isUpdateSent()).isFalse();
-
-        kafkaEnvironment.getBrokers().get(0).start();
-        await().atMost(Duration.ofSeconds(10)).until(() ->
-                kafkaEnvironment.getServerPark().getBrokerStatus() instanceof BrokerStatus.Available);
-
-        jdbcTemplate.update("update generic_storage set last_modified_date = ? where id = ?", LocalDateTime.now().minusMinutes(35), id);
-        teamService.executeCatchupUpdates();
-        await().atMost(Duration.ofSeconds(10)).until(() ->
-                storageService.get(id, Team.class).isUpdateSent());
-
-        var record = KafkaTestUtils.getSingleRecord(consumer, teamUpdateProducer.getTopic());
-        assertThat(record.key()).isEqualTo(savedTeam.getId().toString());
     }
 }

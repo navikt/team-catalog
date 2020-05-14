3092c13ce20ae68b160031d47abfe8177f6b4fdc
Andreas Skomedal <andreas.skomedal@nav.no>
2020-05-14 09:52:17 +0200

    Git version/link

--- apps/frontend/src/components/Navigation/SideBar/index.tsx
+++ apps/frontend/src/components/Navigation/SideBar/index.tsx
@@ -6,6 +6,10 @@ import NavLogo from '../../../resources/navlogo.svg'
 import { StyledLink } from 'baseui/link'
 import NavItem from './NavItem'
 import SlackLogo from '../../../resources/Slack_Monochrome_White.svg'
+import { StatefulTooltip } from 'baseui/tooltip'
+import { env } from '../../../util/env'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCodeBranch } from '@fortawesome/free-solid-svg-icons'
 
 const sideBarProps: BlockProps = {
   position: 'fixed',
@@ -20,7 +24,7 @@ const items: BlockProps = {
 
 const Brand = () => (
   <Block display="flex" flexDirection={"column"} padding="1rem" marginTop="1rem">
-    <StyledLink style={{ textDecoration: 'none', textAlign: 'center' }} href="/">
+    <StyledLink style={{textDecoration: 'none', textAlign: 'center'}} href="/">
       <H6 color="white" marginTop="1rem" marginLeft="5px" marginBottom="2rem">Teamkatalog</H6>
     </StyledLink>
   </Block>
@@ -29,28 +33,34 @@ const Brand = () => (
 const SideBar = () => {
   return (
     <Block {...sideBarProps}>
-      <Brand />
+      <Brand/>
       <Block {...items}>
-        <NavItem to="/productarea" text="Områder" />
-        <NavItem to="/team" text="Teams" />
+        <NavItem to="/productarea" text="Områder"/>
+        <NavItem to="/team" text="Teams"/>
       </Block>
       <Block position="absolute" bottom="0" width="100%">
         <Block display="flex" justifyContent="center">
           <Block paddingBottom={theme.sizing.scale600} width="40%">
-            <img src={NavLogo} alt='NAV logo' width="100%" />
+            <img src={NavLogo} alt='NAV logo' width="100%"/>
           </Block>
         </Block>
-        <a href="slack://channel?team=T5LNAMWNA&id=CG2S8D25D" style={{ textDecoration: 'none' }}>
+        <a href="slack://channel?team=T5LNAMWNA&id=CG2S8D25D" style={{textDecoration: 'none'}}
+           target="_blank" rel="noopener noreferrer">
           <Block display="flex" justifyContent="center" paddingBottom={theme.sizing.scale400} alignItems="center">
-            <img src={SlackLogo} width="60px" alt="slack logo" />
+            <img src={SlackLogo} width="60px" alt="slack logo"/>
             <Paragraph4 color={theme.colors.white}>#datajegerne </Paragraph4>
           </Block>
         </a>
-        <a href="https://dataplattform.gitbook.io/nada/kataloger/teamkatalog" style={{ textDecoration: 'none' }} target="_blank">
-          <Block display="flex" justifyContent="center" paddingBottom={theme.sizing.scale400} alignItems="center">
-            <Paragraph4 color={theme.colors.white}>Dokumentasjon </Paragraph4>
-          </Block>
-        </a>
+        <Block display="flex" justifyContent="center" paddingBottom={theme.sizing.scale400} alignItems="center">
+          <StatefulTooltip content={`Versjon: ${env.githubVersion}`}>
+            <a href='https://github.com/navikt/team-catalog' style={{textDecoration: 'none'}}
+                        target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon color={theme.colors.white} icon={faCodeBranch}/></a>
+          </StatefulTooltip>
+          <a href="https://dataplattform.gitbook.io/nada/kataloger/teamkatalog" style={{textDecoration: 'none'}} target="_blank" rel="noopener noreferrer">
+            <Paragraph4 marginLeft={theme.sizing.scale200} color={theme.colors.white}>Dokumentasjon </Paragraph4>
+          </a>
+        </Block>
       </Block>
     </Block>
   )
--- apps/frontend/src/util/env.ts
+++ apps/frontend/src/util/env.ts
@@ -1,5 +1,6 @@
 export const env = {
   teamCatalogBaseUrl: process.env.REACT_APP_TEAMCATALOG_ENDPOINT,
   amplitudeEndpoint: process.env.REACT_APP_AMPLITUDE_ENDPOINT,
-  amplitudeApiKey: process.env.REACT_APP_AMPLITUDE_API_KEY
+  amplitudeApiKey: process.env.REACT_APP_AMPLITUDE_API_KEY,
+  githubVersion: process.env.REACT_APP_GIT_VERSION || 'local'
 };

556b66982bb508172935a679d2792df39d86bf67
Shayan Yazdanmehr <shayan.yazdanmehr@nav.no>
2020-05-22 13:36:02 +0200

    Improving the design for product area's cards

--- apps/frontend/src/components/ProductArea/ListTeams/CardTeam.tsx
+++ apps/frontend/src/components/ProductArea/ListTeams/CardTeam.tsx
@@ -1,14 +1,27 @@
 import * as React from 'react'
-import { Card, StyledBody } from 'baseui/card';
-import { ProductTeam, Resource } from '../../../constants';
-import { Label2, Paragraph2 } from 'baseui/typography';
-import { Block } from 'baseui/block';
-import { theme } from '../../../util';
-import { useStyletron } from 'styletron-react';
+import {Card, StyledBody} from 'baseui/card';
+import {ProductTeam, Resource} from '../../../constants';
+import {Label2, Paragraph2} from 'baseui/typography';
+import {Block, BlockProps} from 'baseui/block';
+import {theme} from '../../../util';
+import {useStyletron} from 'styletron-react';
 import RouteLink from '../../common/RouteLink'
-import { marginAll } from '../../Style'
-import { intl } from '../../../util/intl/intl'
-import { cardShadow } from '../../common/Style'
+import {marginAll} from '../../Style'
+import {cardShadow} from '../../common/Style'
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faUsers} from "@fortawesome/free-solid-svg-icons";
+
+type CardTeamProps = {
+  team: ProductTeam
+  resource?: Resource
+}
+
+const contentBlockProps: BlockProps = {
+  display: 'flex',
+  justifyContent: 'space-between',
+  width: '100%',
+  marginTop: theme.sizing.scale500,
+}
 
 const TextWithLabel = (props: { label: string, text: string | number }) => (
   <Block display="flex" alignItems="baseline">
@@ -16,11 +29,6 @@ const TextWithLabel = (props: { label: string, text: string | number }) => (
     <Block><Paragraph2 marginBottom="0">{props.text}</Paragraph2></Block>
   </Block>
 )
-
-type CardTeamProps = {
-  team: ProductTeam
-  resource?: Resource
-}
 
 const CardTeam = (props: CardTeamProps) => {
   const [useCss] = useStyletron();
@@ -31,14 +39,42 @@ const CardTeam = (props: CardTeamProps) => {
   return (
 
     <RouteLink href={`/team/${props.team.id}`} className={linkCss}>
-      <Card title={props.team.name} overrides={{
-        Root: {style: {...cardShadow.Root.style, width: '300px', ...marginAll(theme.sizing.scale200)}},
-        Contents: {style: {height: '160px', display: 'flex', flexDirection: 'column', justifyContent: 'space-between'}},
-        Body: {style: {marginBottom: 0}}
-      }}>
+      <Card
+        title={
+            <div>
+              <RouteLink href={`/team/${props.team.id}`} hideUnderline>
+                {props.team.name}
+              </RouteLink>
+            </div>
+        }
+        overrides={{
+          Root: {
+            style: {
+              ...cardShadow.Root.style,
+              width: '450px',
+              ...marginAll(theme.sizing.scale200),
+            }
+          },
+          Body: {
+            style: {
+              marginBottom: 0
+            }
+          },
+          Title:{
+            style:{
+              height:'50px'
+            }
+          }
+        }}>
         <StyledBody>
-          {member && <TextWithLabel label="Rolle" text={member.roles.map(r => intl[r]).join(", ")}/>}
-          {!member && <TextWithLabel label="Medlemmer" text={props.team.members.length}/>}
+          <Block  {...contentBlockProps}>
+            <Block flex={1}>
+              {!member && <TextWithLabel label="Medlemmer" text={props.team.members.length}/>}
+            </Block>
+            <Block flex='0 0 50px'>
+              <FontAwesomeIcon icon={faUsers} size='2x' color={theme.colors.accent300} />
+            </Block>
+          </Block>
         </StyledBody>
       </Card>
     </RouteLink>

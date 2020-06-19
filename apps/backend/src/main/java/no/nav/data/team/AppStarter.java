dc7e6ba735775c94c921c867ce2d6d0ff4283a54
Andreas Skomedal <andreas.skomedal@nav.no>
2020-06-19 14:37:55 +0200

    List vault files

--- apps/backend/src/main/java/no/nav/data/team/AppStarter.java
+++ apps/backend/src/main/java/no/nav/data/team/AppStarter.java
@@ -6,9 +6,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.stream.Stream;
 
 @Slf4j
 @ConfigurationPropertiesScan
@@ -21,8 +23,18 @@ public class AppStarter {
     }
 
     private static void readAzureSecret() {
+        listSecretFiles();
         fileToProp("/var/run/secrets/nais.io/srv/username", "SRV_USER");
         fileToProp("/var/run/secrets/nais.io/srv/password", "SRV_PASSWORD");
+    }
+
+    private static void listSecretFiles() {
+        var base = Paths.get("/var/run/secrets/nais.io");
+        try (Stream<Path> paths = Files.walk(base)) {
+            paths.forEach(p -> log.info("Vault file: {}", p.toAbsolutePath()));
+        } catch (IOException e) {
+            log.error("couldnt list vault files", e);
+        }
     }
 
     private static void fileToProp(String file, String prop) {

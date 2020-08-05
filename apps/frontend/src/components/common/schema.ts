b3f7febf2c77b4acc4d42e7827086a998c7a6b0a
Andreas Skomedal <andreas.skomedal@nav.no>
2020-08-05 13:47:15 +0200

    Upgrade frontend dependencies

--- apps/frontend/src/components/common/schema.ts
+++ apps/frontend/src/components/common/schema.ts
@@ -1,5 +1,6 @@
-import * as yup from "yup";
-import { MemberFormValues, ProductAreaFormValues, ProductTeamFormValues, TeamRole, TeamType } from "../../constants";
+import * as yup from 'yup';
+import {Schema} from 'yup';
+import {MemberFormValues, ProductAreaFormValues, ProductTeamFormValues, TeamRole, TeamType} from '../../constants';
 
 const errorMessage = "Feltet er påkrevd";
 
@@ -7,8 +8,8 @@ export const productAreaSchema = () =>
   yup.object<ProductAreaFormValues>({
     name: yup.string().required(errorMessage),
     description: yup.string().required(errorMessage),
-    members: yup.array(memberSchema()),
-    tags: yup.array(yup.string())
+    members: yup.array().of(memberSchema()),
+    tags: yup.array().of(yup.string())
   });
 
 export const teamSchema = () =>
@@ -18,16 +19,18 @@ export const teamSchema = () =>
     productAreaId: yup.string(),
     description: yup.string().required(errorMessage),
     slackChannel: yup.string(),
-    naisTeams: yup.array(yup.string()),
-    members: yup.array(memberSchema()),
+    naisTeams: yup.array().of(yup.string()),
+    members: yup.array().of(memberSchema()),
     teamLeadQA: yup.boolean(),
     teamType: yup.mixed().oneOf(Object.values(TeamType), errorMessage).required(errorMessage),
-    tags: yup.array(yup.string())
+    tags: yup.array().of(yup.string())
   });
+
+const roleSchema: Schema<TeamRole> = yup.mixed().oneOf(Object.values(TeamRole), errorMessage + ": Rolle").required(errorMessage)
 
 export const memberSchema = () =>
   yup.object<MemberFormValues>({
     navIdent: yup.string().required(errorMessage + ": Ansatt"),
-    roles: yup.array(yup.mixed().oneOf(Object.values(TeamRole), errorMessage + ": Rolle")).required(errorMessage + ": Rolle"),
+    roles: yup.array().of(roleSchema).required(errorMessage + ": Rolle"),
     description: yup.string(),
   });

5d4d40d13b1d9acb586e454ddbd01edd85e0ce67
Andreas Skomedal <andreas.skomedal@nav.no>
2020-08-06 14:35:19 +0200

    main page skeleton vs spinners

--- apps/frontend/src/pages/MainPage.tsx
+++ apps/frontend/src/pages/MainPage.tsx
@@ -7,7 +7,6 @@ import {Dashboard} from '../components/dash/Dashboard'
 import {Card} from 'baseui/card'
 import {cardShadow} from '../components/common/Style'
 import {theme} from '../util'
-import {Spinner} from '../components/common/Spinner'
 import {getAllTeams} from '../api'
 import {ProductTeam} from '../constants'
 import {ListItemLabel} from 'baseui/list/index'
@@ -15,6 +14,7 @@ import moment from 'moment'
 import {HeadingSmall, LabelXSmall} from 'baseui/typography/index'
 import RouteLink from '../components/common/RouteLink'
 import {marginAll} from '../components/Style'
+import {Skeleton} from 'baseui/skeleton/index'
 
 const contentProps: BlockProps = {
   width: "100%",
@@ -36,66 +36,72 @@ const MainPage = () => {
 const MainPageMessage = () => {
   const [settings, setSettings] = useState<Settings>()
   useEffect(() => {
-    (async () => {
-      setSettings(await getSettings())
-    })()
+    getSettings().then(setSettings)
   }, [])
 
   return (
     <>
-      {!settings && <Block marginLeft={theme.sizing.scale1200}><Spinner size={theme.sizing.scale2400}/></Block>}
-      {!!settings && <Block marginTop={theme.sizing.scale600}>
+      <Block marginTop={theme.sizing.scale600}>
         <Card overrides={cardShadow}>
           <ReactMarkdown source={settings?.frontpageMessage} escapeHtml={false}/>
+          {!settings && <Skeleton width='500px' rows={20} animation/>}
         </Card>
-      </Block>}
+      </Block>
     </>
   )
 }
 
 const RecentTeams = () => {
   const [teamList, setTeamList] = useState<ProductTeam[]>()
+  const numTeams = 12
 
   useEffect(() => {
     getAllTeams().then(r => {
       const content = r.content
       content.sort((a, b) => moment(b.changeStamp.lastModifiedDate).valueOf() - moment(a.changeStamp.lastModifiedDate).valueOf())
-      setTeamList(content.slice(0, 12))
+      setTeamList(content.slice(0, numTeams))
     })
   }, [])
-
-  if (!teamList) return null
 
   return (
     <Block>
       <HeadingSmall marginBottom={theme.sizing.scale600}>Sist endrede teams</HeadingSmall>
       <Block flexWrap display='flex' justifyContent='space-between' marginLeft={`-${theme.sizing.scale300}`}>
-        {teamList.map(team =>
+        {teamList && teamList.map(team =>
           <RouteLink key={team.id} href={`/team/${team.id}`}>
-            <Card overrides={{
-              Root: {
-                style: {
-                  ...cardShadow.Root.style,
-                  width: '220px',
-                  marginTop: theme.sizing.scale800,
-                  marginLeft: theme.sizing.scale300
-                }
-              }, Contents: {
-                style: {
-                  ...marginAll(theme.sizing.scale400)
-                }
-              }
-            }}>
-              <Block display='flex' flexDirection='column' justifyContent='space-between' height='72px'>
-                <ListItemLabel><span style={{wordBreak: 'break-word'}}>{team.name}</span></ListItemLabel>
-                <LabelXSmall marginTop={theme.sizing.scale300}>Endret: {moment(team.changeStamp.lastModifiedDate).format('lll')}</LabelXSmall>
-              </Block>
-            </Card>
+            <TeamCard>
+              <ListItemLabel><span style={{wordBreak: 'break-word'}}>{team.name}</span></ListItemLabel>
+              <LabelXSmall marginTop={theme.sizing.scale300}>Endret: {moment(team.changeStamp.lastModifiedDate).format('lll')}</LabelXSmall>
+            </TeamCard>
           </RouteLink>
+        )}
+        {!teamList && Array.from(Array(numTeams).keys()).map(i =>
+          <TeamCard><Skeleton key={i} width='100%' rows={3} animation/></TeamCard>
         )}
       </Block>
     </Block>
   )
 }
+
+const TeamCard = (props: {children: React.ReactNode}) => (
+  <Card overrides={{
+    Root: {
+      style: {
+        ...cardShadow.Root.style,
+        width: '220px',
+        marginTop: theme.sizing.scale800,
+        marginLeft: theme.sizing.scale300
+      }
+    }, Contents: {
+      style: {
+        ...marginAll(theme.sizing.scale400)
+      }
+    }
+  }}>
+    <Block display='flex' flexDirection='column' justifyContent='space-between' height='72px'>
+      {props.children}
+    </Block>
+  </Card>
+)
 
 export default MainPage

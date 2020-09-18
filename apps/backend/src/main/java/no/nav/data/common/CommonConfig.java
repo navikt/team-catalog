f92f3bfaed30417c300819d6352b2a128bfaaeeb
Andreas Skomedal <andreas.skomedal@nav.no>
2020-09-18 10:24:14 +0200

    DefaultExports again?

--- apps/backend/src/main/java/no/nav/data/common/CommonConfig.java
+++ apps/backend/src/main/java/no/nav/data/common/CommonConfig.java
@@ -2,6 +2,7 @@ package no.nav.data.common;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.prometheus.client.CollectorRegistry;
+import io.prometheus.client.hotspot.DefaultExports;
 import lombok.extern.slf4j.Slf4j;
 import net.javacrumbs.shedlock.core.LockProvider;
 import net.javacrumbs.shedlock.provider.jdbctemplate.JdbcTemplateLockProvider;
@@ -59,6 +60,7 @@ public class CommonConfig {
      */
     @Bean
     public CollectorRegistry collectorRegistry() {
+        DefaultExports.initialize();
         return CollectorRegistry.defaultRegistry;
     }
 

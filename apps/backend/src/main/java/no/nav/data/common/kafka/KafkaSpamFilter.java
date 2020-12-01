615804d342d65782f3f899b584ba2f047c5ff05d
Andreas Skomedal <andreas.skomedal@nav.no>
2020-12-01 16:51:23 +0100

    KafkaSpamFilter

---
+++ apps/backend/src/main/java/no/nav/data/common/kafka/KafkaSpamFilter.java
@@ -0,0 +1,17 @@
+package no.nav.data.common.kafka;
+
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.filter.Filter;
+import ch.qos.logback.core.spi.FilterReply;
+
+public class KafkaSpamFilter extends Filter<ILoggingEvent> {
+
+    @Override
+    public FilterReply decide(ILoggingEvent event) {
+        if ("The configuration 'specific.avro.reader' was supplied but isn't a known config.".equals(event.getMessage()) ||
+                "The configuration 'schema.registry.url' was supplied but isn't a known config.".equals(event.getMessage())) {
+            return FilterReply.DENY;
+        }
+        return FilterReply.NEUTRAL;
+    }
+}

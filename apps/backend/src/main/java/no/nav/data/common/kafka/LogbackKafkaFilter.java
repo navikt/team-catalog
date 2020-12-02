a493731519652538ebf604ea78167b180ad4eb87
Andreas Skomedal <andreas.skomedal@nav.no>
2020-12-02 10:20:02 +0100

    enable preview for layertool

--- apps/backend/src/main/java/no/nav/data/common/kafka/LogbackKafkaFilter.java
+++ apps/backend/src/main/java/no/nav/data/common/kafka/LogbackKafkaFilter.java
@@ -4,7 +4,7 @@ import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.filter.Filter;
 import ch.qos.logback.core.spi.FilterReply;
 
-public class KafkaSpamFilter extends Filter<ILoggingEvent> {
+public class LogbackKafkaFilter extends Filter<ILoggingEvent> {
 
     @Override
     public FilterReply decide(ILoggingEvent event) {

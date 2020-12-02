a2a0478e2b36cc08699781a50bd6bc874bc0ae3c
Andreas Skomedal <andreas.skomedal@nav.no>
2020-12-02 10:37:16 +0100

    Fix kafka log filter

--- apps/backend/src/main/java/no/nav/data/common/kafka/LogbackKafkaFilter.java
+++ apps/backend/src/main/java/no/nav/data/common/kafka/LogbackKafkaFilter.java
@@ -8,8 +8,9 @@ public class LogbackKafkaFilter extends Filter<ILoggingEvent> {
 
     @Override
     public FilterReply decide(ILoggingEvent event) {
-        if ("The configuration 'specific.avro.reader' was supplied but isn't a known config.".equals(event.getMessage()) ||
-                "The configuration 'schema.registry.url' was supplied but isn't a known config.".equals(event.getMessage())) {
+        var msg = event.getFormattedMessage();
+        if ("The configuration 'specific.avro.reader' was supplied but isn't a known config.".equals(msg) ||
+                "The configuration 'schema.registry.url' was supplied but isn't a known config.".equals(msg)) {
             return FilterReply.DENY;
         }
         return FilterReply.NEUTRAL;

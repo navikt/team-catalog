9c1bac62cbba81db10cea3393c4bbc996bca09e9
Andreas Skomedal <andreas.skomedal@nav.no>
2021-02-22 11:07:25 +0100

    Interactive tree

--- apps/frontend/src/components/graph/TreeMap.tsx
+++ apps/frontend/src/components/graph/TreeMap.tsx
@@ -1,4 +1,4 @@
-import {ResponsiveTreeMapHtml} from '@nivo/treemap'
+import {NodeEventHandler, ResponsiveTreeMap} from '@nivo/treemap'
 import React, {useEffect, useState} from 'react'
 import {useAllProductAreas, useAllTeams} from '../../api'
 import {Block} from 'baseui/block'
@@ -12,49 +12,76 @@ export const Treemap = () => {
   const teams = useAllTeams()
   const areas = useAllProductAreas()
   const [data, setData] = useState<Node>()
+  const [focusPath, setFocusPath] = useState<string | undefined>()
+  const [focusArea, setFocusArea] = useState(false)
+
+  const isArea = (area: string) => focusPath?.indexOf(`NAV.${area}`) === 0
+  const isTeam = (area: string, team: string) => focusPath?.indexOf(`NAV.${area}.${team}`) === 0
 
   useEffect(() => {
+    console.log(focusArea, focusPath)
     setData({
       name: 'NAV',
-      children: areas.map(a => ({
-        name: a.name,
-        children: teams.filter(t => t.productAreaId === a.id).map(t => ({
-          name: t.name,
-          shortName: formatTeamName(t.name),
-          value: t.members.length,
-          // children: t.members.map(m => ({
-          //   name: m.resource.fullName || m.navIdent,
-          //   value: 1
-          // }))
-        })).filter(t => !!t.value)
-        //})).filter(t => !!t.children.length)
-      })).filter(a => !!a.children.length)
+      type: 'root',
+      children: areas.map(a => {
+        const ateams = teams.filter(t => t.productAreaId === a.id)
+        return ({
+          name: a.name,
+          formatName: `${a.name} (${ateams.length})`,
+          type: 'area',
+          children: (isArea(a.name) || !focusArea ? ateams.map(t => ({
+            name: t.name,
+            formatName: formatTeamName(t.name),
+            value: t.members.length,
+            type: 'team',
+            children: isTeam(a.name, t.name) ? t.members.map(m => ({
+              name: m.resource.fullName || m.navIdent,
+              value: 1,
+              type: 'res'
+            })) : []
+          })) : []).filter(t => !!t.value)
+        })
+      }).filter(a => !!a.children.length)
     })
-  }, [teams, areas])
+  }, [teams, areas, focusPath])
 
   return (
     <Block width='100%' height='800px'>
-      {data && <Map data={data}/>}
+      {data && <Map data={data} onClick={(node) => {
+        const type = (node.data as Node).type
+        if (type === 'area') {
+          setFocusArea(!focusArea)
+        }
+        const newFocus = node.path === focusPath ? undefined : node.path
+        if (type == 'root' || !newFocus) setFocusArea(false)
+        setFocusPath(newFocus)
+      }}/>}
       {!data && <Spinner size={theme.sizing.scale800}/>}
     </Block>
   )
 }
 
-const Map = (props: {data: Node}) => (
-  <ResponsiveTreeMapHtml
+const Map = (props: {data: Node, onClick: NodeEventHandler}) => (
+  <ResponsiveTreeMap
     data={props.data}
     identity='name'
-    label={((node: {data: Node, value: number}) => node.data.shortName || node.data.name) as any}
+    label={((node: {data: Node}) => node.data.formatName || node.data.name) as any}
+    parentLabel={((node: {data: Node}) => node.data.formatName || node.data.name) as any}
     parentLabelSize={30}
     parentLabelPadding={12}
     labelSkipSize={0}
+    onClick={props.onClick}
+    innerPadding={5}
+    outerPadding={5}
+    colors={{scheme: 'paired'}}
   />
 )
 
 type Node = {
   name: string
-  shortName?: string
+  formatName?: string
   color?: string
   children?: Node[]
   value?: any
+  type: string
 }

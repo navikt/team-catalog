654278326798be1b118f49346335f3a34d058f14
Andreas Skomedal <andreas.skomedal@nav.no>
2021-04-08 11:28:45 +0200

    AdminController annotations

--- apps/backend/src/main/java/no/nav/data/team/settings/AdminController.java
+++ apps/backend/src/main/java/no/nav/data/team/settings/AdminController.java
@@ -3,6 +3,7 @@ package no.nav.data.team.settings;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import no.nav.data.common.mail.EmailService;
@@ -16,6 +17,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
@@ -23,6 +25,7 @@ import java.util.UUID;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "Admin", description = "Admin ops")
 @RequestMapping("/admin")
 public class AdminController {
 
@@ -41,7 +44,7 @@ public class AdminController {
     @Operation(summary = "trigger nudge")
     @ApiResponses(value = {@ApiResponse(description = "nudge")})
     @PostMapping(value = "/nudge", produces = "text/html")
-    public ResponseEntity<String> nudge(UUID teamId) {
+    public ResponseEntity<String> nudge(@RequestParam(value = "teamId") UUID teamId) {
         var team = storage.get(teamId, Team.class);
         notificationService.nudge(team);
         return ResponseEntity.ok("ok");

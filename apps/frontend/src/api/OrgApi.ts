80da0efab70e8e2cf5266e57f86384c7ab4f28d9
Andreas Harnes <65889446+aharnes-nav@users.noreply.github.com>
2021-07-21 12:00:45 +0200

    Org main page update og NOM graphql queries update

--- apps/frontend/src/api/OrgApi.ts
+++ apps/frontend/src/api/OrgApi.ts
@@ -1,48 +1,42 @@
-import {useEffect, useState} from 'react'
-import axios from "axios"
-import { env } from 'process'
-import { OrgEnhet, HierarkiData } from '../pages/OrgMainPage'
-
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { env } from "process";
+import { OrgEnhet, HierarkiData } from "../pages/OrgMainPage";
 
 const getOrg = async (orgId: string) => {
-  const baseURL = env.teamCatalogBaseUrl ?? "/api"
-  return (await axios.get<OrgEnhet>(`${baseURL}/org/${orgId}`)).data
-}
+  const baseURL = env.teamCatalogBaseUrl ?? "/api";
+  return (await axios.get<OrgEnhet>(`${baseURL}/org/${orgId}`)).data;
+};
 
-
-const getHierarki:(orgId: string) => Promise<HierarkiData[]> = async (orgId: string) => {
-  const org = await getOrg(orgId)
-  const overenhet = org.organiseringer.find(o => o.retning === "over")
-    if(overenhet){
-      const overenhetData: HierarkiData = {
-        navn:overenhet.organisasjonsenhet.navn,
-        id:overenhet.organisasjonsenhet.agressoId
-      }
-      const returnData = await getHierarki(overenhet.organisasjonsenhet.agressoId)
-      return [...returnData, overenhetData]
-    }
-  return [] as HierarkiData[]
-}
-
+const getHierarki: (orgId: string) => Promise<HierarkiData[]> = async (orgId: string) => {
+  const org = await getOrg(orgId);
+  const overenhet = org.organiseringer.find((o) => o.retning === "over");
+  if (overenhet) {
+    const overenhetData: HierarkiData = {
+      navn: overenhet.organisasjonsenhet.navn,
+      id: overenhet.organisasjonsenhet.agressoId,
+    };
+    const returnData = await getHierarki(overenhet.organisasjonsenhet.agressoId);
+    return [...returnData, overenhetData];
+  }
+  return [] as HierarkiData[];
+};
 
 export const useOrg = (orgId: string) => {
-  const [org, setOrg] = useState<OrgEnhet>()
-  const [orgHierarki, setOrgHierarki] = useState<HierarkiData[]>([])
-
+  const [org, setOrg] = useState<OrgEnhet>();
+  const [orgHierarki, setOrgHierarki] = useState<HierarkiData[]>([]);
 
   useEffect(() => {
-    let orgId2 = 'NAV'
-    if(orgId){
-      orgId2 = orgId
+    if (!orgId) {
+      orgId = "NAV";
     }
-    getOrg(orgId2).then(r => {
-      setOrg(r)
-    })
+    getOrg(orgId).then((r) => {
+      setOrg(r);
+    });
 
-    getHierarki(orgId2).then(x => {
-      setOrgHierarki(x)
-    })
-
-  }, [orgId])
-  return {org, orgHierarki}
-}
+    getHierarki(orgId).then((x) => {
+      setOrgHierarki(x);
+    });
+  }, [orgId]);
+  return { org, orgHierarki };
+};
--- apps/frontend/src/pages/OrgMainPage.tsx
+++ apps/frontend/src/pages/OrgMainPage.tsx
@@ -1,101 +1,114 @@
-import * as React from 'react'
-import {Block} from "baseui/block";
-import {useOrg} from "../api/OrgApi";
-import {Redirect, useParams} from "react-router-dom";
+import * as React from "react";
+import { Block } from "baseui/block";
+import { useOrg } from "../api/OrgApi";
+import { Redirect, useParams } from "react-router-dom";
 import PageTitle from "../components/common/PageTitle";
-import {TextWithLabel} from "../components/common/TextWithLabel";
-import {H1} from "baseui/typography";
-import {Card} from "baseui/card";
+import { TextWithLabel } from "../components/common/TextWithLabel";
+import { H1 } from "baseui/typography";
+import { Card } from "baseui/card";
 import RouteLink from "../components/common/RouteLink";
-import {cardShadow} from "../components/common/Style";
-import {marginAll} from "../components/Style";
-import {theme} from "../util";
-
+import { cardShadow } from "../components/common/Style";
+import { marginAll } from "../components/Style";
+import { theme } from "../util";
 
 export interface OrgEnhet {
-  agressoId: string
-  navn: string
-  gyldigFom?: string
-  gyldigTom?: string
-  organiseringer: OrgEnhetOrganisering[]
+  agressoId: string;
+  navn: string;
+  gyldigFom?: string;
+  gyldigTom?: string;
+  organiseringer: OrgEnhetOrganisering[];
 }
 
 export interface OrgEnhetOrganisering {
-  retning: "over" | "under"
-  organisasjonsenhet: OrgEnhet
+  retning: "over" | "under";
+  organisasjonsenhet: OrgEnhet;
 }
 
 export interface HierarkiData {
-  navn: string,
-  id: string
+  navn: string;
+  id: string;
 }
 
-
-const OrgEnhetCard = (props: { navn: string, id: string }) => {
-  const linkUri = "/org/" + props.id
-  return <Card key={props.id} overrides={{
-    Root:
-      {
-        style:
-          {
+const OrgEnhetCard = (props: { navn: string; id: string }) => {
+  const linkUri = "/org/" + props.id;
+  return (
+    <Card
+      key={props.id}
+      overrides={{
+        Root: {
+          style: {
             ...cardShadow.Root.style,
-            width: '450px',
+            width: "450px",
             ...marginAll(theme.sizing.scale200),
-          }
-      }
-  }}><H1>{props.navn}</H1>
-    <RouteLink href={linkUri}>Gå til enhet</RouteLink>
-  </Card>
-}
+          },
+        },
+      }}
+    >
+      <H1>{props.navn}</H1>
+      <RouteLink href={linkUri}>Gå til enhet</RouteLink>
+    </Card>
+  );
+};
 
-const OrgHierarki = (props: { navn: string, id: string }) => {
-  const hierarkiData = props
-  return <div>
-    <RouteLink href={hierarkiData.id}>{hierarkiData.navn}</RouteLink>
-  </div>
-
-
-}
+const OrgHierarki = (props: { navn: string; id: string; cIndex: number }) => {
+  return (
+    <div>
+      <RouteLink href={props.id} style={{ paddingLeft: String(props.cIndex * 27) + "px" }}>
+        {props.navn}
+      </RouteLink>
+    </div>
+  );
+};
 
 export const OrgMainPage = () => {
-  const {id: orgId} = useParams<any>()
-  const {org, orgHierarki} = useOrg(orgId);
+  const { id: orgId } = useParams<any>();
+  const { org, orgHierarki } = useOrg(orgId);
 
-  if(orgId === undefined){
-    return <Redirect to={"/org/NAV"}></Redirect>
+  if (orgId === undefined) {
+    return <Redirect to={"/org/NAV"} />;
   }
-  if(!org){
-    return<div>Laster</div>
+  if (!org) {
+    return <div>Laster</div>;
   }
 
-  const oe:OrgEnhet = org as OrgEnhet
-  const underenheter: { navn: string, id: string }[] = oe.organiseringer.filter(oee => oee.retning === "under").map(ue => {
-    return {navn: ue.organisasjonsenhet.navn, id: ue.organisasjonsenhet.agressoId}
-  })
+  const oe: OrgEnhet = org as OrgEnhet;
+  const underenheter: { navn: string; id: string }[] = oe.organiseringer
+    .filter((oee) => oee.retning === "under")
+    .map((ue) => {
+      return { navn: ue.organisasjonsenhet.navn, id: ue.organisasjonsenhet.agressoId };
+    });
 
-  const overenheter: { navn: string, id: string }[] = oe.organiseringer.filter(oee => oee.retning === "over").map(ue => {
-    return {navn: ue.organisasjonsenhet.navn, id: ue.organisasjonsenhet.agressoId}
-  })
-  const ingenOverenhet = overenheter.length === 0
-
-
+  const overenheter: { navn: string; id: string }[] = oe.organiseringer
+    .filter((oee) => oee.retning === "over")
+    .map((ue) => {
+      return { navn: ue.organisasjonsenhet.navn, id: ue.organisasjonsenhet.agressoId };
+    });
+  const ingenOverenhet = overenheter.length === 0;
+  const ingenUnderenheter = underenheter.length === 0;
 
   return (
     <Block>
-      <PageTitle title={oe.navn}/>
-      {ingenOverenhet ? null :
+      <PageTitle title={oe.navn} />
+      {ingenOverenhet ? null : (
         <Block>
-          <TextWithLabel label={"NAV hierarki:"} text={orgHierarki.map(hierarkiData =>
-            <OrgHierarki key={hierarkiData.id} navn={hierarkiData.navn} id={hierarkiData.id}/>
-          )}/>
+          <TextWithLabel
+            label={"NAV hierarki:"}
+            text={orgHierarki.map((hierarkiData, index) => (
+              <OrgHierarki key={hierarkiData.id} navn={hierarkiData.navn} id={hierarkiData.id} cIndex={index} />
+            ))}
+          />
+        </Block>
+      )}
 
-        </Block> }
-
-      <Block display="flex" flexWrap>
-        {underenheter.map(ue =>
-          <OrgEnhetCard key={ue.id} navn={ue.navn} id={ue.id}/>
-        )}
-      </Block>
+      {ingenUnderenheter ? (
+        <Block>{/* Insert enhets info her */}</Block>
+      ) : (
+        <Block display="flex" flexWrap>
+          {underenheter.map((ue) => (
+            <OrgEnhetCard key={ue.id} navn={ue.navn} id={ue.id} />
+          ))}
+        </Block>
+      )}
     </Block>
-  )
-}
+  );
+};

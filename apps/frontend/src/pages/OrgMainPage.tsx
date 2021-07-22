8e1949374325f0cbe18d04b47d8155adf69d03c3
Andreas Harnes <65889446+aharnes-nav@users.noreply.github.com>
2021-07-22 19:20:51 +0200

    Update OrgMainPage.tsx
    Lagt inn data fra nom på siden og lagt inn basic styling på det

--- apps/frontend/src/pages/OrgMainPage.tsx
+++ apps/frontend/src/pages/OrgMainPage.tsx
@@ -4,12 +4,13 @@ import { useOrg } from "../api/OrgApi";
 import { Redirect, useParams } from "react-router-dom";
 import PageTitle from "../components/common/PageTitle";
 import { TextWithLabel } from "../components/common/TextWithLabel";
-import { H1 } from "baseui/typography";
+import { H1, Paragraph2 } from "baseui/typography";
 import { Card } from "baseui/card";
 import RouteLink from "../components/common/RouteLink";
 import { cardShadow } from "../components/common/Style";
 import { marginAll } from "../components/Style";
 import { theme } from "../util";
+import { concat } from "lodash";
 
 export interface OrgEnhet {
   agressoId: string;
@@ -17,6 +18,32 @@ export interface OrgEnhet {
   gyldigFom?: string;
   gyldigTom?: string;
   organiseringer: OrgEnhetOrganisering[];
+  leder: Ressurs[];
+  koblinger: Kobling[];
+}
+
+interface Ressurs {
+  ressurs: PersonInfo;
+}
+
+interface PersonInfo {
+  nomId?: any;
+  navIdent: any;
+  personIdent?: any;
+  koblinger?: Kobling[];
+  lederFor?: any;
+  ledere?: any[];
+  person?: any;
+  epost?: any;
+  visningsNavn?: any;
+  fornavn?: any;
+  etternavn?: any;
+}
+
+interface Kobling {
+  ressurs: PersonInfo;
+  gyldigFom?: string;
+  gyldigTom?: string;
 }
 
 export interface OrgEnhetOrganisering {
@@ -60,6 +87,44 @@ const OrgHierarki = (props: { navn: string; id: string; cIndex: number }) => {
   );
 };
 
+const OrgEnhetInfo = (props: { enhetsnavn: string; agressoId: string; enhetsType: string }) => {
+  return (
+    <div style={{ width: "50rem" }}>
+      <Block width="50%">
+        <Paragraph2>
+          Enhetsnavn: <label>{props.enhetsnavn}</label>
+        </Paragraph2>
+        <Paragraph2>
+          AgressoId: <label>{props.agressoId}</label>
+        </Paragraph2>
+        <Paragraph2>
+          Enhetstype: <label>{props.enhetsType}</label>
+        </Paragraph2>
+      </Block>
+    </div>
+  );
+};
+
+const OrgLeder = (props: { navn: string; navIdent: string }) => {
+  return (
+    <div>
+      <Paragraph2>
+        {props.navn} (<RouteLink href={"/resource/".concat(props.navIdent)}>{props.navIdent}</RouteLink>)
+      </Paragraph2>
+    </div>
+  );
+};
+
+const OrgRessurs = (props: { navn: string; navIdent: string }) => {
+  return (
+    <div>
+      <Paragraph2>
+        {props.navn} (<RouteLink href={"/resource/".concat(props.navIdent)}>{props.navIdent}</RouteLink>)
+      </Paragraph2>
+    </div>
+  );
+};
+
 export const OrgMainPage = () => {
   const { id: orgId } = useParams<any>();
   const { org, orgHierarki } = useOrg(orgId);
@@ -72,6 +137,7 @@ export const OrgMainPage = () => {
   }
 
   const oe: OrgEnhet = org as OrgEnhet;
+  console.log(oe);
   const underenheter: { navn: string; id: string }[] = oe.organiseringer
     .filter((oee) => oee.retning === "under")
     .map((ue) => {
@@ -83,6 +149,16 @@ export const OrgMainPage = () => {
     .map((ue) => {
       return { navn: ue.organisasjonsenhet.navn, id: ue.organisasjonsenhet.agressoId };
     });
+
+  const orgLederInfo: { navn: string; navIdent: string }[] = oe.leder.map((lInfo) => {
+    return { navn: lInfo.ressurs.visningsNavn, navIdent: lInfo.ressurs.navIdent };
+  });
+
+  const orgRessurs: { navn: string; navIdent: string }[] = oe.koblinger.map((or) => {
+    return { navn: or.ressurs.visningsNavn, navIdent: or.ressurs.navIdent };
+  });
+
+  console.log({ or_info: orgRessurs });
   const ingenOverenhet = overenheter.length === 0;
   const ingenUnderenheter = underenheter.length === 0;
 
@@ -101,7 +177,32 @@ export const OrgMainPage = () => {
       )}
 
       {ingenUnderenheter ? (
-        <Block>{/* Insert enhets info her */}</Block>
+        <Block>
+          <Block display="flex" width="80%" marginTop="4em">
+            {
+              <TextWithLabel
+                width="50%"
+                label={"Info"}
+                text={<OrgEnhetInfo enhetsnavn={oe.navn} agressoId={oe.agressoId} enhetsType={"enhetstype"}></OrgEnhetInfo>}
+              ></TextWithLabel>
+            }
+
+            <TextWithLabel
+              width="40%"
+              label={"Leder"}
+              text={orgLederInfo.map((lederData) => (
+                <OrgLeder key={lederData.navIdent} navn={lederData.navn} navIdent={lederData.navIdent} />
+              ))}
+            ></TextWithLabel>
+          </Block>
+          <TextWithLabel
+            marginTop="8em"
+            label={"Ressurser"}
+            text={orgRessurs.map((rData) => (
+              <OrgRessurs key={rData.navIdent} navn={rData.navn} navIdent={rData.navIdent}></OrgRessurs>
+            ))}
+          ></TextWithLabel>
+        </Block>
       ) : (
         <Block display="flex" flexWrap>
           {underenheter.map((ue) => (

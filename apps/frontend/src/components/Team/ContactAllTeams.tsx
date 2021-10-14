15fc12c0f01d0fc29de5b5272cb1abad121f5433
Andreas Harnes <65889446+aharnes-nav@users.noreply.github.com>
2021-10-14 19:23:33 +0200

    Update ContactAllTeams.tsx
    added support for contact person

--- apps/frontend/src/components/Team/ContactAllTeams.tsx
+++ apps/frontend/src/components/Team/ContactAllTeams.tsx
@@ -14,7 +14,6 @@ function validateContactAddresses(contactAddresses: ContactAddress[]) {
   }
   return state
 }
-
 function getEmail(contactAddresses: ContactAddress[]) {
   let email = ''
   contactAddresses.forEach((element) => {
@@ -24,40 +23,33 @@ function getEmail(contactAddresses: ContactAddress[]) {
   })
   return email
 }
-
-function contactTeams(props: ProductTeam[]) {
+const contactTeams = async (productTeams: ProductTeam[]) => {
   let emails = ''
-  let tempEmails: string[] = []
-  let currentEmail = ''
-  props.forEach(function (element) {
-    currentEmail = getContactAddress(element)
-    if (currentEmail != '') {
-      tempEmails.push(currentEmail)
-    }
-    currentEmail = ''
-  })
 
-  tempEmails = tempEmails.filter((v, i, a) => a.indexOf(v) === i)
-  tempEmails.forEach(function (elements) {
-    emails = emails.concat(elements + '; ')
-  })
+  const arrayEmails = (
+    await Promise.all(
+      productTeams.map((pt: ProductTeam) => {
+        return getContactAddress(pt)
+      })
+    )
+  )
+    .filter((n) => n)
+    .map((email) => {
+      emails += email + '; '
+    })
+
   document.location.href = 'mailto:' + emails
 }
 
-// TODO Mangler kontakt person
-function getContactAddress(prop: ProductTeam) {
-  const teamLeader = prop.members.filter((tLeader) => tLeader.roles.includes(TeamRole.LEAD)) ?? null
-  const productOWner = prop.members.filter((tLeader) => tLeader.roles.includes(TeamRole.PRODUCT_OWNER)) ?? null
-
+const getContactAddress = async (productTeam: ProductTeam) => {
+  const teamLeader = productTeam.members.filter((tLeader) => tLeader.roles.includes(TeamRole.LEAD)) ?? null
+  const productOWner = productTeam.members.filter((po) => po.roles.includes(TeamRole.PRODUCT_OWNER)) ?? null
   let contactAddress = ''
-
-  if (prop.contactPersonIdent) {
-    const res = async (ident: string) => {
-      const user = await getResourceById(prop.contactPersonIdent || '')
-    }
-    console.log({ res })
-  } else if (prop.contactAddresses.length != 0 && validateContactAddresses(prop.contactAddresses)) {
-    contactAddress = getEmail(prop.contactAddresses)
+  if (productTeam.contactPersonIdent) {
+    const res = await getResourceById(productTeam.contactPersonIdent)
+    contactAddress = res.email
+  } else if (productTeam.contactAddresses.length != 0 && validateContactAddresses(productTeam.contactAddresses)) {
+    contactAddress = getEmail(productTeam.contactAddresses)
   } else if (teamLeader.length != 0) {
     teamLeader.forEach(function (value) {
       contactAddress = value.resource.email || ''
@@ -69,7 +61,6 @@ function getContactAddress(prop: ProductTeam) {
   }
   return contactAddress
 }
-
 export const ContactAllTeams = (props: { teams: ProductTeam[] }) => {
   return (
     <Button tooltip="Kontakt alle team" icon={faMailBulk} kind="outline" size="compact" marginLeft onClick={() => contactTeams(props.teams)}>

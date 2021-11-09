0929a1c7a7dd80db8cf4a40b2f401457d4c0b650
Andreas Harnes <65889446+aharnes-nav@users.noreply.github.com>
2021-11-09 13:00:31 +0100

    Update index.tsx
    Added comment for future bug fixing

--- apps/frontend/src/components/ProductArea/List/index.tsx
+++ apps/frontend/src/components/ProductArea/List/index.tsx
@@ -1,13 +1,13 @@
 import * as React from 'react'
-import {Cluster, ProductArea, ProductTeam, Resource} from '../../../constants'
+import { Cluster, ProductArea, ProductTeam, Resource } from '../../../constants'
 import CardTeam from './CardTeam'
 import CardProductArea from './CardProductArea'
-import {Block} from 'baseui/block'
-import {Label1, Paragraph2} from 'baseui/typography'
-import {theme} from '../../../util'
-import {TeamExport} from '../../Team/TeamExport'
+import { Block } from 'baseui/block'
+import { Label1, Paragraph2 } from 'baseui/typography'
+import { theme } from '../../../util'
+import { TeamExport } from '../../Team/TeamExport'
 import CardCluster from './CardCluster'
-import {useLocation} from "react-router-dom";
+import { useLocation } from 'react-router-dom'
 
 type ListMembersProps = {
   teams?: ProductTeam[]
@@ -19,55 +19,62 @@ type ListMembersProps = {
 }
 
 export const CardList = (props: ListMembersProps) => {
-  const location = useLocation();
-  return <>
-    {props.teams &&
-    <Block>
-      <Block display='flex' justifyContent='space-between'>
-        <Label1 marginBottom={theme.sizing.scale800}>Team ({props.teams.length})</Label1>
-        {location.pathname.split("/")[1] !== "resource" && <TeamExport productAreaId={props.productAreaId} clusterId={props.clusterId}/>}
-      </Block>
-      {props.teams.length ?
-        <Block
-          display='flex'
-          flexWrap
-        >
-          {props.teams.map((team: ProductTeam) => (
-            <CardTeam key={team.id} team={team} resource={props.resource}/>
-          ))}
+  const location = useLocation()
+  return (
+    <>
+      {props.teams && (
+        <Block>
+          <Block display="flex" justifyContent="space-between">
+            <Label1 marginBottom={theme.sizing.scale800}>Team ({props.teams.length})</Label1>
+            {location.pathname.split('/')[1] !== 'resource' && <TeamExport productAreaId={props.productAreaId} clusterId={props.clusterId} />}
+          </Block>
+          {props.teams.length ? (
+            <Block display="flex" flexWrap>
+              {props.teams.map((team: ProductTeam) => (
+                <CardTeam key={team.id} team={team} resource={props.resource} />
+              ))}
+            </Block>
+          ) : (
+            <Paragraph2>Ingen team</Paragraph2>
+          )}
         </Block>
-        : <Paragraph2>Ingen team</Paragraph2>}
-    </Block>}
+      )}
 
-    {props.clusters &&
-    <Block marginTop={theme.sizing.scale1200}>
-      <Label1 marginBottom={theme.sizing.scale800}>Klynger ({props.clusters.length})</Label1>
-      {props.clusters.length ?
-        <Block
-          display='flex'
-          flexWrap
-        >
-          {props.clusters?.map(cl => (
-            <CardCluster key={cl.id} cluster={cl} resource={props.resource}
-                         teams={props.teams?.filter(t => t.clusterIds.indexOf(cl.id) >= 0)}/>
-          ))}
+      {props.clusters && (
+        <Block marginTop={theme.sizing.scale1200}>
+          <Label1 marginBottom={theme.sizing.scale800}>Klynger ({props.clusters.length})</Label1>
+          {props.clusters.length ? (
+            <Block display="flex" flexWrap>
+              {props.clusters?.map((cl) => (
+                <CardCluster
+                  key={cl.id}
+                  cluster={cl}
+                  resource={props.resource}
+                  // TODO props team er subset av det vi trenger. Hadde props.team inneholdt alle team i teamkat ville dette fungert
+                  teams={props.teams?.filter((t) => t.clusterIds.indexOf(cl.id) >= 0)}
+                />
+              ))}
+            </Block>
+          ) : (
+            <Paragraph2>Ingen klynger</Paragraph2>
+          )}
         </Block>
-        : <Paragraph2>Ingen klynger</Paragraph2>}
-    </Block>}
+      )}
 
-    {props.productAreas &&
-    <Block marginTop={theme.sizing.scale1200}>
-      <Label1 marginBottom={theme.sizing.scale800}>Områder ({props.productAreas.length})</Label1>
-      {props.productAreas.length ?
-        <Block
-          display='flex'
-          flexWrap
-        >
-          {props.productAreas?.map((pa: ProductArea) => (
-            <CardProductArea key={pa.id} productArea={pa} resource={props.resource} teams={props.teams}/>
-          ))}
+      {props.productAreas && (
+        <Block marginTop={theme.sizing.scale1200}>
+          <Label1 marginBottom={theme.sizing.scale800}>Områder ({props.productAreas.length})</Label1>
+          {props.productAreas.length ? (
+            <Block display="flex" flexWrap>
+              {props.productAreas?.map((pa: ProductArea) => (
+                <CardProductArea key={pa.id} productArea={pa} resource={props.resource} teams={props.teams} />
+              ))}
+            </Block>
+          ) : (
+            <Paragraph2>Ingen områder</Paragraph2>
+          )}
         </Block>
-        : <Paragraph2>Ingen områder</Paragraph2>}
-    </Block>}
-  </>
+      )}
+    </>
+  )
 }

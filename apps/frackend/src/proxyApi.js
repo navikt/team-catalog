bcc502554ae06d03cad82197c90c7731fce94cb8
Bent <bent.holden@nav.no>
2022-01-26 22:27:57 +0100

    Frackend: fixed backend target ingress env

--- apps/frackend/src/proxyApi.js
+++ apps/frackend/src/proxyApi.js
@@ -3,7 +3,7 @@ import { createProxyMiddleware } from 'http-proxy-middleware';
 function setupProxy(app) {
     // Proxy endpoints
     app.use('/team-catalog', createProxyMiddleware({
-        target: process.env.TARGET_URL,
+        target: process.env.TEAM_CATALOG_BACKEND_URL,
         changeOrigin: true,
         onProxyReq: (proxyReq => proxyReq.removeHeader('authorization')),
         pathRewrite: {

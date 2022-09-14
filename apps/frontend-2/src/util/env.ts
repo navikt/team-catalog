b5d1d6f5c65eec8493efdf0a3e8eecc94cc58670
danishnav <54104284+danishnav@users.noreply.github.com>
2022-09-14 11:58:12 +0200

    Fixed env - vite way

--- apps/frontend-2/src/util/env.ts
+++ apps/frontend-2/src/util/env.ts
@@ -1,9 +1,9 @@
 export const env = {
-  teamCatalogBaseUrl: process.env.REACT_APP_TEAMCATALOG_ENDPOINT,
-  nomApiBaseUrl: process.env.REACT_APP_NOMAPI_ENDPOINT,
-  amplitudeEndpoint: process.env.REACT_APP_AMPLITUDE_ENDPOINT,
-  amplitudeApiKey: process.env.REACT_APP_AMPLITUDE_API_KEY,
-  githubVersion: process.env.REACT_APP_GIT_VERSION || 'local',
-  processCatBaseUrl: process.env.REACT_APP_PROCESS_CAT_BASE_URL,
+  teamCatalogBaseUrl: import.meta.env.REACT_APP_TEAMCATALOG_ENDPOINT,
+  nomApiBaseUrl: import.meta.env.REACT_APP_NOMAPI_ENDPOINT,
+  amplitudeEndpoint: import.meta.env.REACT_APP_AMPLITUDE_ENDPOINT,
+  amplitudeApiKey: import.meta.env.REACT_APP_AMPLITUDE_API_KEY,
+  githubVersion: import.meta.env.REACT_APP_GIT_VERSION || 'local',
+  processCatBaseUrl: import.meta.env.REACT_APP_PROCESS_CAT_BASE_URL,
   isSandbox: window.location.host.indexOf('sandbox') >= 0
 };

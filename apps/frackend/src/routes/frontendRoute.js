28c449dda50aba8d0b9b1676fd81ba3235b9d91f
erik-a-e <65284304+erik-a-e@users.noreply.github.com>
2022-09-20 16:33:44 +0200

    webapp-beta: cookie based beta webapp toggle

--- apps/frackend/src/routes/frontendRoute.js
+++ apps/frackend/src/routes/frontendRoute.js
@@ -4,14 +4,42 @@ import * as fs from "fs";
 
 
 const indexHtmlExists = fs.existsSync("./public/index.html");
+const betaIndexHtmlExists = fs.existsSync("./public2/index.html")
+
 const publicFolderPath = indexHtmlExists ? "./public" : "./publicLocal";
+const betaPublicFolderPath = betaIndexHtmlExists ? "./public2" : "./publicLocal"
+
+const webappStaticHandler = express.static(publicFolderPath);
+const betaWebappStaticHandler = express.static(betaPublicFolderPath)
+
+const checkBetaEnabledCookie = req => (req.headers.cookie || "").includes("BETA-ENABLED");
 
 const setupStaticRoutes = (app) => {
 
-    app.use(express.static(publicFolderPath))
+    app.get("/beta-on",(req,res, next) => {
+        res.cookie('BETA-ENABLED','ON')
+        res.status(200).send('<a href="/">BETA-ENABLED</a>')
+    })
+
+    app.get("/beta-off",(req,res, next) => {
+        res.clearCookie('BETA-ENABLED')
+        res.status(200).send('<a href="/">BETA-DISABLED</a>')
+    })
+
+    app.use((req,res,next) => {
+        if(checkBetaEnabledCookie(req)) {
+            betaWebappStaticHandler(req,res,next)
+        }else{
+            webappStaticHandler(req,res,next)
+        }
+    })
 
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(publicFolderPath) + "/index.html");
+        if(checkBetaEnabledCookie(req)){
+            res.sendFile(path.resolve(betaPublicFolderPath) + "/index.html")
+        }else{
+            res.sendFile(path.resolve(publicFolderPath) + "/index.html");
+        }
     });
 }
 

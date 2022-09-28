1900020d5dcf34e432270c33b2e9636b0a24739d
danishnav <54104284+danishnav@users.noreply.github.com>
2022-09-28 09:37:30 +0200

    Satt opp route og page for resourcePage

--- apps/frontend-2/src/routes.tsx
+++ apps/frontend-2/src/routes.tsx
@@ -3,6 +3,7 @@ import { Routes, Route, Navigate } from 'react-router-dom'
 
 import Root from './components/Root'
 import MainPage from './pages/MainPage'
+import ResourcePage from './pages/ResourcePage'
 import TeamListPage from './pages/TeamListPage'
 import TeamPage from './pages/TeamPage'
 
@@ -10,8 +11,11 @@ const MainRoutes = (): JSX.Element => (
   <Root>
     <Routes>
       <Route path="/" element={<MainPage />} />
+
       <Route path="/team" element={<TeamListPage />} />
       <Route path="/team/:id" element={<TeamPage/>} />
+
+      <Route path="/resource/:id" element={<ResourcePage />} />
     </Routes>
   </Root>
 )

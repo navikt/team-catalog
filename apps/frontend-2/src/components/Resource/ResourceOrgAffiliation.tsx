a4dd5fe848d406a6276cb43b11d832fb5d59fa94
Johannes <johannes.moskvil@gmail.com>
2022-11-10 12:02:51 +0100

    Refactor resourcePage to use react-query

--- apps/frontend-2/src/components/Resource/ResourceOrgAffiliation.tsx
+++ apps/frontend-2/src/components/Resource/ResourceOrgAffiliation.tsx
@@ -1,20 +1,26 @@
 import { BodyShort } from "@navikt/ds-react";
+import { useQuery } from "react-query";
 
-import type { Resource, ResourceUnits } from "../../constants";
+import { getResourceUnitsById } from "../../api";
+import type { Resource } from "../../constants";
 import { ResourceInfoContainer } from "../common/ResourceInfoContainer";
 
 type ResourceOrgAffiliationProperties = {
   resource: Resource;
-  units?: ResourceUnits;
 };
 
-const ResourceOrgAffiliation = (properties: ResourceOrgAffiliationProperties) => {
-  const { units } = properties;
+const ResourceOrgAffiliation = ({ resource }: ResourceOrgAffiliationProperties) => {
+  const fetchResourceUnitsQuery = useQuery({
+    queryKey: ["getResourceUnitsById", resource.navIdent],
+    queryFn: () => getResourceUnitsById(resource.navIdent),
+  });
+
+  const units = fetchResourceUnitsQuery.data?.units ?? [];
 
   return (
     <ResourceInfoContainer title="Organisatorisk tilhørighet">
-      {(units?.units.length ?? 0) === 0 && <BodyShort>Personen har ingen organisatorisk tilhørighet</BodyShort>}
-      {(units?.units.length ?? 0) > 0 && (
+      {(units.length ?? 0) === 0 && <BodyShort>Personen har ingen organisatorisk tilhørighet</BodyShort>}
+      {(units.length ?? 0) > 0 && (
         <div>
           Under arbeid
           {/* <TextWithLabel label='Ansatt i' text={<Link to={`/org/${agressoIdDataToUrl(u.id, u.niva || '')}`}>{u.name}</Link>} /> */}

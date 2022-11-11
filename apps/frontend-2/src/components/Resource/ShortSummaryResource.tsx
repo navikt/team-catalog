5ff35dd4626699290647383c57bbcd5529becf18
Johannes <johannes.moskvil@gmail.com>
2022-11-11 11:59:32 +0100

    Add sluttdato and only show if it is in the past

--- apps/frontend-2/src/components/Resource/ShortSummaryResource.tsx
+++ apps/frontend-2/src/components/Resource/ShortSummaryResource.tsx
@@ -27,16 +27,20 @@ const ShortSummaryResource = (properties: AboutUsSectionProperties) => {
         <img alt="Lokasjon" src={identIcon} />
         <TextWithLabel label="NAV-ident" text={resource.navIdent} />
       </div>
-
       <div className={containerCss}>
         <img alt="E-post" src={emailIcon} />
         <TextWithLabel label="E-post" text={resource.email} />
       </div>
-
       <div className={containerCss}>
         <img alt="Startdato" src={calendarIcon} />
         <TextWithLabel label="Startdato" text={dayjs(resource.startDate).format("DD.MM.YYYY")} />
       </div>
+      {dayjs(resource.endDate).isBefore(new Date()) && (
+        <div className={containerCss}>
+          <img alt="Sluttdato" src={calendarIcon} />
+          <TextWithLabel label="Sluttdato" text={dayjs(resource.endDate).format("DD.MM.YYYY")} />
+        </div>
+      )}
     </ResourceInfoContainer>
   );
 };

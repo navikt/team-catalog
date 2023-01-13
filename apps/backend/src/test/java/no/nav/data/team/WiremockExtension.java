c38d2b55b91acb274ccc76f34405a1016f848581
Fredrik Hov <37329363+frehov@users.noreply.github.com>
2023-01-13 14:14:32 +0100

    ORG-903: Update naisteam tests to pass, remove some nora properties and types

--- apps/backend/src/test/java/no/nav/data/team/WiremockExtension.java
+++ apps/backend/src/test/java/no/nav/data/team/WiremockExtension.java
@@ -6,9 +6,7 @@ import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
 import no.nav.data.common.rest.RestResponsePage;
 import no.nav.data.team.integration.process.dto.PcatCode;
 import no.nav.data.team.integration.process.dto.PcatProcess;
-import no.nav.data.team.naisteam.nora.NoraApp;
-import no.nav.data.team.naisteam.nora.NoraMember;
-import no.nav.data.team.naisteam.nora.NoraTeam;
+import no.nav.data.team.naisteam.console.ConsoleTeam;
 import org.junit.jupiter.api.extension.AfterEachCallback;
 import org.junit.jupiter.api.extension.BeforeAllCallback;
 import org.junit.jupiter.api.extension.BeforeEachCallback;
@@ -16,13 +14,16 @@ import org.junit.jupiter.api.extension.Extension;
 import org.junit.jupiter.api.extension.ExtensionContext;
 
 import java.util.List;
+import java.util.Map;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.get;
 import static com.github.tomakehurst.wiremock.client.WireMock.notFound;
 import static com.github.tomakehurst.wiremock.client.WireMock.ok;
 import static com.github.tomakehurst.wiremock.client.WireMock.okJson;
+import static com.github.tomakehurst.wiremock.client.WireMock.post;
 import static com.github.tomakehurst.wiremock.client.WireMock.put;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlMatching;
+import static java.util.Collections.emptyList;
 import static no.nav.data.common.utils.JsonUtils.toJson;
 
 public class WiremockExtension implements Extension, BeforeAllCallback, BeforeEachCallback, AfterEachCallback {
@@ -49,11 +50,7 @@ public class WiremockExtension implements Extension, BeforeAllCallback, BeforeEa
     }
 
     private void stubCommon() {
-        getWiremock().stubFor(get("/nora/teams").willReturn(okJson(toJson(noraMockResponse()))));
-        getWiremock().stubFor(get("/nora/teams/nais-team-1").willReturn(okJson(toJson(noraTeam("nais-team-1")))));
-        getWiremock().stubFor(get("/nora/teams/nais-team-2").willReturn(okJson(toJson(noraTeam("nais-team-2")))));
-        getWiremock().stubFor(get("/nora/apps/nais-team-1").willReturn(okJson(toJson(List.of(noraApp("app1"), noraApp("app2"))))));
-        getWiremock().stubFor(get("/nora/apps/nais-team-2").willReturn(okJson(toJson(List.of(noraApp("app3"))))));
+        getWiremock().stubFor(post("/console/query").willReturn(okJson(toJson(consoleMockResponse())))); // Stub Nais Console Team Query
 
         getWiremock().stubFor(get(urlMatching("/datacatgraph/node/out/.*")).willReturn(notFound()));
         getWiremock().stubFor(get(urlMatching("/datacatgraph/node/in/.*")).willReturn(notFound()));
@@ -66,16 +63,21 @@ public class WiremockExtension implements Extension, BeforeAllCallback, BeforeEa
         return WIREMOCK;
     }
 
-    private List<NoraTeam> noraMockResponse() {
-        return List.of(noraTeam("nais-team-1"), noraTeam("nais-team-2"), NoraTeam.builder().name("xteamR").build());
+    private Map<String, Object> consoleMockResponse() {
+        // TODO: Improve graphql response.
+        // This is a very rudimentary response made to only fulfill the expectations of the tests from NaisTeamIT
+        return Map.of(
+                "data", Map.of(
+                        "teams", List.of(consoleTeam("nais-team-1"), consoleTeam("nais-team-2"), consoleTeam("xteamR")),
+                        "team", consoleTeam("nais-team-1")
+                ),
+                "errors", emptyList()
+        );
+//        return List.of(consoleTeam("nais-team-1"), consoleTeam("nais-team-2"), consoleTeam("xteamR"));
     }
 
-    private NoraTeam noraTeam(String teamname) {
-        return NoraTeam.builder().name(teamname).members(List.of(NoraMember.builder().name("Member Name").email("member@email.com").build())).build();
-    }
-
-    private NoraApp noraApp(String appnName) {
-        return NoraApp.builder().name("Visual " + appnName).zone("fss").build();
+    private ConsoleTeam consoleTeam(String teamname) {
+        return new ConsoleTeam(true, "", "", teamname);
     }
 
     private void mockBkat() {

8f9d012cb809ebafa3f6fb78bf4f824b5da8c8bb
Fredrik Hov <37329363+frehov@users.noreply.github.com>
2023-01-13 10:33:47 +0100

    ORG-903: First pass at reimplementing lookup of nais teams, this time from nais console application

---
+++ apps/backend/src/main/java/no/nav/data/team/naisteam/console/ConsoleTeam.java
@@ -0,0 +1,62 @@
+package no.nav.data.team.naisteam.console;
+
+
+import no.nav.data.team.naisteam.domain.NaisTeam;
+
+import static java.util.Collections.emptyList;
+
+public record ConsoleTeam (
+        boolean enabled,
+        String slackAlertsChannel,
+        String purpose,
+        String slug
+) implements Toggleable, Named {
+
+    @SuppressWarnings("GraphQLUnresolvedReference")
+    public final static String TEAMS_QUERY = //language=graphql
+            """
+            query {
+              teams {
+                enabled
+                slackAlertsChannel
+                purpose
+                slug
+              }
+            }
+            """;
+
+    @SuppressWarnings("GraphQLUnresolvedReference")
+    public final static String TEAM_QUERY = // language=graphql
+            """
+            query($slug: String!) {
+              team(slug: $slug) {
+                enabled
+                slackAlertsChannel
+                purpose
+                slug
+              }
+            }
+            """;
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    @Override
+    public String getName() {
+        return slug;
+    }
+
+    public NaisTeam toNaisTeam() {
+        return NaisTeam.builder()
+                .id(getName())
+                .name(getName())
+                .description(purpose())
+                .slack(slackAlertsChannel())
+                .naisMembers(emptyList())
+                .naisApps(emptyList())
+                .build();
+    }
+
+}

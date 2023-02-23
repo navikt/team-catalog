0c52c243730126ba74308d5ede4e0a4a6a1bf989
Roy Grini <roy.grini@nav.no>
2023-02-23 21:18:45 +0100

    Added aspect proxy, and Teimed/Counted Aspect beans

--- apps/backend/src/main/java/no/nav/data/common/CommonConfig.java
+++ apps/backend/src/main/java/no/nav/data/common/CommonConfig.java
@@ -1,6 +1,9 @@
 package no.nav.data.common;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.micrometer.core.aop.CountedAspect;
+import io.micrometer.core.aop.TimedAspect;
+import io.micrometer.core.instrument.MeterRegistry;
 import io.prometheus.client.CollectorRegistry;
 import lombok.extern.slf4j.Slf4j;
 import net.javacrumbs.shedlock.core.LockProvider;
@@ -8,18 +11,18 @@ import net.javacrumbs.shedlock.provider.jdbctemplate.JdbcTemplateLockProvider;
 import no.nav.data.common.utils.JsonUtils;
 import no.nav.data.common.web.TraceHeaderRequestInterceptor;
 import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.*;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.client.RestTemplate;
 
 import javax.sql.DataSource;
 
+import static java.util.Collections.emptyList;
+
 @Slf4j
 @Configuration
+@EnableAspectJAutoProxy
 public class CommonConfig {
 
     @Primary
@@ -66,4 +69,19 @@ public class CommonConfig {
                 .build());
     }
 
+    @Bean
+    TimedAspect timedAspect(MeterRegistry meterRegistry) {
+        return new TimedAspect(meterRegistry,
+                proceedingJoinPoint -> emptyList(),
+                proceedingJoinPoint -> false
+        );
+    }
+
+    @Bean
+    CountedAspect countedAspect(MeterRegistry meterRegistry) {
+        return new CountedAspect(meterRegistry,
+                proceedingJoinPoint -> emptyList(),
+                proceedingJoinPoint -> false
+        );
+    }
 }

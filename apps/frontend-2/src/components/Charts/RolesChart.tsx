22126abd92670d5501df6de47c06788256c08962
Johannes <johannes.moskvil@gmail.com>
2023-04-11 15:36:01 +0200

    Remove wrong css

--- apps/frontend-2/src/components/Charts/RolesChart.tsx
+++ apps/frontend-2/src/components/Charts/RolesChart.tsx
@@ -1,128 +1,132 @@
-import {css} from "@emotion/css";
-import {Fragment} from "react";
-import {createMemo} from "react-use";
-import {Bar, BarChart, LabelList, XAxis, YAxis} from "recharts";
-
-import type {Cluster, Member, ProductArea, ProductTeam} from "../../constants";
-import {Status, TeamRole} from "../../constants";
-import {useAllClusters} from "../../hooks";
-import {useAllProductAreas} from "../../hooks";
-import {useAllTeams} from "../../hooks";
-import {intl} from "../../util/intl/intl";
+import { css } from "@emotion/css";
+import partition from "lodash/partition";
 import sortBy from "lodash/sortBy";
-import {partition, sumBy} from "lodash";
-import {useNavigate} from "react-router-dom";
+import sumBy from "lodash/sumBy";
+import { Fragment } from "react";
+import { useNavigate } from "react-router-dom";
+import { createMemo } from "react-use";
+import { Bar, BarChart, LabelList, XAxis, YAxis } from "recharts";
+
+import type { Cluster, Member, ProductArea, ProductTeam } from "../../constants";
+import { Status, TeamRole } from "../../constants";
+import { useAllClusters } from "../../hooks";
+import { useAllProductAreas } from "../../hooks";
+import { useAllTeams } from "../../hooks";
+import { intl } from "../../util/intl/intl";
 
 // NOTE 16 Nov 2022 (Johannes Moskvil): BarChart data must be memoized for LabelList to render correctly with animations
 const useMemoTeamMembersData = createMemo(formatData);
 
 export function RolesChart() {
-    const teams = useAllTeams({status: Status.ACTIVE});
-    const areas = useAllProductAreas({status: Status.ACTIVE});
-    const clusters = useAllClusters({status: Status.ACTIVE});
+  const teams = useAllTeams({ status: Status.ACTIVE });
+  const areas = useAllProductAreas({ status: Status.ACTIVE });
+  const clusters = useAllClusters({ status: Status.ACTIVE });
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const memoizedData = useMemoTeamMembersData(teams.data ?? [], areas.data ?? [], clusters.data ?? []);
-    console.log(memoizedData)
-    if (memoizedData.length === 0) {
-        return <></>;
-    }
+  const memoizedData = useMemoTeamMembersData(teams.data ?? [], areas.data ?? [], clusters.data ?? []);
+  console.log(memoizedData);
+  if (memoizedData.length === 0) {
+    return <></>;
+  }
 
-    return (
-        <Fragment>
-            <h2>Andel personer per rolle</h2>
-            <div
-                className={css`
-                  background: #e6f1f8;
-                  padding: 2rem;
-                  width: max-content;
-                  margin-bottom: 2rem;
+  return (
+    <Fragment>
+      <h2>Andel personer per rolle</h2>
+      <div
+        className={css`
+          background: #e6f1f8;
+          padding: 2rem;
+          width: max-content;
+          margin-bottom: 2rem;
 
-                  .recharts-bar-rectangle {
-                    cursor: pointer;
-                  }
-                `}
-            >
-                <BarChart
-                    barCategoryGap={2}
-                    barGap={4}
-                    barSize={25}
-                    data={memoizedData}
-                    height={1200}
-                    layout="vertical"
-                    width={600}
-                >
-                    <Bar css={css`color: red; cursor: pointer;`} dataKey="numberOfMembers" fill="#005077" onClick={(event) => {
-                        if (event.roleEnum) {
-                            navigate(`/dashboard/members/role/${event.roleEnum}`)
-                        }
-                    }} radius={3}
-                         width={30}>
-                        <LabelList dataKey="numberOfMembers" position="right"/>
-                    </Bar>
-                    <XAxis hide type="number"/>
-                    <YAxis axisLine={false} dataKey="role" tickLine={false} type="category" width={200}/>
-                </BarChart>
-            </div>
-        </Fragment>
-    );
+          .recharts-bar-rectangle {
+            cursor: pointer;
+          }
+        `}
+      >
+        <BarChart
+          barCategoryGap={2}
+          barGap={4}
+          barSize={25}
+          data={memoizedData}
+          height={1200}
+          layout="vertical"
+          width={600}
+        >
+          <Bar
+            dataKey="numberOfMembers"
+            fill="#005077"
+            onClick={(event) => {
+              if (event.roleEnum) {
+                navigate(`/dashboard/members/role/${event.roleEnum}`);
+              }
+            }}
+            radius={3}
+            width={30}
+          >
+            <LabelList dataKey="numberOfMembers" position="right" />
+          </Bar>
+          <XAxis hide type="number" />
+          <YAxis axisLine={false} dataKey="role" tickLine={false} type="category" width={200} />
+        </BarChart>
+      </div>
+    </Fragment>
+  );
 }
 
 function formatData(teams: ProductTeam[], areas: ProductArea[], clusters: Cluster[]) {
-    const allMembers = getAllMembers(teams, areas, clusters);
-    const sortedRoles = sortRoles(allMembers);
+  const allMembers = getAllMembers(teams, areas, clusters);
+  const sortedRoles = sortRoles(allMembers);
 
-    const sortedRolesCombined = combineSmallValues(sortedRoles);
+  const sortedRolesCombined = combineSmallValues(sortedRoles);
 
-    return sortedRolesCombined.map((roleWithCount) => {
-        return formatDataRow(roleWithCount, allMembers);
-    });
+  return sortedRolesCombined.map((roleWithCount) => {
+    return formatDataRow(roleWithCount, allMembers);
+  });
 }
 
 function formatDataRow(row: ChartDataRow, allMembers: Member[]) {
-    const percentage = Math.round((row.numberOfMembers / allMembers.length) * 100);
+  const percentage = Math.round((row.numberOfMembers / allMembers.length) * 100);
 
-    return {
-        ...row,
-        roleText: `${row.role} (${percentage}%)`,
-    };
+  return {
+    ...row,
+    roleText: `${row.role} (${percentage}%)`,
+  };
 }
 
 type ChartDataRow = {
-    role: string;
-    enumRole?: TeamRole;
-    numberOfMembers: number;
-}
+  role: string;
+  enumRole?: TeamRole;
+  numberOfMembers: number;
+};
 
-function combineSmallValues(
-    dataRows: ChartDataRow[]
-) {
-    const [rows, rowsToBeSquashed] = partition(dataRows, row => row.numberOfMembers >= 30);
+function combineSmallValues(dataRows: ChartDataRow[]) {
+  const [rows, rowsToBeSquashed] = partition(dataRows, (row) => row.numberOfMembers >= 30);
 
-    if (rowsToBeSquashed.length > 0) {
-        rows.push({
-            role: "Diverse mindre roller",
-            numberOfMembers: sumBy(rowsToBeSquashed, "numberOfMembers"),
-        });
-    }
-    return rows;
+  if (rowsToBeSquashed.length > 0) {
+    rows.push({
+      role: "Diverse mindre roller",
+      numberOfMembers: sumBy(rowsToBeSquashed, "numberOfMembers"),
+    });
+  }
+  return rows;
 }
 
 function sortRoles(members: Member[]) {
-    const enumRoles = Object.keys(TeamRole) as TeamRole[];
-    const output = enumRoles.map((role) => {
-        const numberOfMembersWithRole = members.filter((member) => member.roles.includes(role));
-        return {role: intl[role], roleEnum: role, numberOfMembers: numberOfMembersWithRole.length};
-    });
+  const enumRoles = Object.keys(TeamRole) as TeamRole[];
+  const output = enumRoles.map((role) => {
+    const numberOfMembersWithRole = members.filter((member) => member.roles.includes(role));
+    return { role: intl[role], roleEnum: role, numberOfMembers: numberOfMembersWithRole.length };
+  });
 
-    return sortBy(output, "numberOfMembers");
+  return sortBy(output, "numberOfMembers");
 }
 
 function getAllMembers(teams: ProductTeam[], areas: ProductArea[], clusters: Cluster[]) {
-    const teamMembers = teams.flatMap((team) => team.members);
-    const areaMembers = areas.flatMap((area) => area.members);
-    const clusterMembers = clusters.flatMap((cluster) => cluster.members);
+  const teamMembers = teams.flatMap((team) => team.members);
+  const areaMembers = areas.flatMap((area) => area.members);
+  const clusterMembers = clusters.flatMap((cluster) => cluster.members);
 
-    return [...teamMembers, ...areaMembers, ...clusterMembers];
+  return [...teamMembers, ...areaMembers, ...clusterMembers];
 }

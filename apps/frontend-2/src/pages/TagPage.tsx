553ad321cea65f8248f59ff70656fdc28e478ea4
danishnav <54104284+danishnav@users.noreply.github.com>
2023-04-18 11:14:10 +0200

    Lagt til lenker for navn på tag table

--- apps/frontend-2/src/pages/TagPage.tsx
+++ apps/frontend-2/src/pages/TagPage.tsx
@@ -1,7 +1,7 @@
-import { useAllProductAreas, useAllTeams } from "../hooks";
-import { ProductArea, ProductTeam, Status } from "../constants";
+import { useAllClusters, useAllProductAreas, useAllTeams } from "../hooks";
+import { ProductArea, ProductTeam, Status, Cluster } from "../constants";
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { SortState, Table } from "@navikt/ds-react";
 import { PageHeader } from "../components/PageHeader";
 import { css } from "@emotion/css";
@@ -13,6 +13,7 @@ export enum TagType {
 }
 
 type TagListType = {
+    id: string,
     name: string,
     type: TagType,
     description: string
@@ -30,82 +31,63 @@ const TagPage = () => {
     const productAreaQuery = useAllProductAreas({ status });
     const productAreas = productAreaQuery.data ?? [];
 
-    // const clusterQuery = useQuery({
-    //     queryKey: ["getAllClusters", status],
-    //     queryFn: () => getAllClusters({ status }),
-    //     select: (data: any) => data.content,
-    // });
-    // const clusters = clusterQuery.data ?? [];
+    const clusterQuery = useAllClusters({ status })
+    const clusters = clusterQuery.data ?? [];
 
-    const handleSort = (sortKey: string) => {
-        setSort(
-          sort && sortKey === sort.orderBy && sort.direction === "descending"
-            ? undefined
-            : {
-                orderBy: sortKey,
-                direction:
-                  sort && sortKey === sort.orderBy && sort.direction === "ascending"
-                    ? "descending"
-                    : "ascending",
-              }
-        );
-      };
+    const getUrl = (item: TagListType) => {
+        if (item.type === TagType.AREA) return `/area/${item.id}`
 
-      const sortedList = (list: TagListType[]) => {
-        return list.slice().sort((a, b) => {
-            if (sort) {
-              const comparator = (a, b: any, orderBy: any) => {
-                if (b[orderBy] < a[orderBy] || b[orderBy] === undefined) {
-                  return -1;
-                }
-                if (b[orderBy] > a[orderBy]) {
-                  return 1;
-                }
-                return 0;
-              };
-        
-              return sort.direction === "ascending"
-                ? comparator(b, a, sort.orderBy)
-                : comparator(a, b, sort.orderBy);
-            }
-            return 1;
-        });
-      }
+        if (item.type === TagType.TEAM) 
+            return `/team/${item.id}`
+        else 
+            return `/cluster/${item.id}`
+    }
 
     useEffect(() => {
         setDisplayList([])
         let filteredTeams: TagListType[] = teams.filter((t: ProductTeam) => params.id && t.tags.includes(params.id)).map((ft: ProductTeam) => ({
+            id: ft.id,
             name: ft.name,
             type: TagType.TEAM,
             description: ft.description
         }))
         let filteredAreas: TagListType[] = productAreas.filter((p: ProductArea) => params.id && p.tags.includes(params.id)).map((fp: ProductArea) => ({
+            id: fp.id,
             name: fp.name,
             type: TagType.AREA,
             description: fp.description
         }))
 
-        setDisplayList([...filteredTeams, ...filteredAreas])
+        let filteredClusters: TagListType[] = clusters.filter((c: Cluster) => params.id && c.tags.includes(params.id)).map((c: Cluster) => ({
+            id: c.id,
+            name: c.name,
+            type: TagType.CLUSTER,
+            description: c.description
+        }))
+
+        setDisplayList([...filteredTeams, ...filteredAreas, ...filteredClusters])
     }, [params]);
 
     return (
         <div>
             <PageHeader title={`Tagg: ${params.id}`} />
 
-            <Table className={css`margin-top: 1rem;`} sort={sort} onSortChange={(sortKey) => handleSort(sortKey || "name")}>
+            <Table className={css`margin-top: 1rem;`}>
                 <Table.Header>
                     <Table.Row>
-                        <Table.ColumnHeader sortKey="name" sortable scope="col">Navn</Table.ColumnHeader>
-                        <Table.ColumnHeader sortKey="type" sortable scope="col">Type</Table.ColumnHeader>
+                        <Table.ColumnHeader scope="col">Navn</Table.ColumnHeader>
+                        <Table.ColumnHeader scope="col">Type</Table.ColumnHeader>
                         <Table.ColumnHeader scope="col">Beskrivelse</Table.ColumnHeader>
 
                     </Table.Row>
                 </Table.Header>
                 <Table.Body>
-                    {sortedList(displayList).map((item: TagListType, i) => {
+                    {displayList.map((item: TagListType, i) => {
                     return (
                         <Table.Row key={i + item.name}>
-                            <Table.HeaderCell scope="row" className={css`width: 20%;`}>{item.name}</Table.HeaderCell>
+                            <Table.DataCell className={css`width: 20%;`}>
+                                <Link to={getUrl(item)}>{item.name}</Link>
+                            </Table.DataCell>
                             <Table.DataCell className={css`width: 25%;`}>{item.type}</Table.DataCell>
                             <Table.DataCell>{item.description}</Table.DataCell>
                         </Table.Row>

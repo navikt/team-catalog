6774fca1c7022acce597b266bde30205b7b37dde
Johannes <johannes.moskvil@gmail.com>
2023-04-25 10:23:35 +0200

    Simplify TeamsSection.tsx

--- apps/frontend-2/src/components/team/TeamsSection.tsx
+++ apps/frontend-2/src/components/team/TeamsSection.tsx
@@ -1,40 +1,11 @@
 import { css } from "@emotion/css";
-import { Button, Heading } from "@navikt/ds-react";
-import { Fragment } from "react";
+import { Heading } from "@navikt/ds-react";
 
 import type { ProductTeam } from "../../constants";
 import { CardContainer, TeamCard } from "../common/Card";
 import { TeamExport } from "../common/TeamExport";
 
 export function TeamsSection({ teams }: { teams: ProductTeam[] }) {
-  if (teams.length === 0) {
-    return (
-      <Fragment>
-        <div
-          className={css`
-            display: flex;
-            justify-content: space-between;
-            margin-bottom: 2rem;
-          `}
-        >
-          <Heading
-            className={css`
-              margin-right: 2rem;
-              margin-top: 0;
-            `}
-            level={"2"}
-            size="medium"
-          >
-            Team ({teams.length})
-          </Heading>
-          <Button disabled size="medium" variant="secondary">
-            Eksporter team
-          </Button>
-        </div>
-        <p>Ingen team i klyngen. Klyngen knyttes til teamene via teamsiden.</p>
-      </Fragment>
-    );
-  }
   return (
     <>
       <div
@@ -49,18 +20,22 @@ export function TeamsSection({ teams }: { teams: ProductTeam[] }) {
             margin-right: 2rem;
             margin-top: 0;
           `}
-          level={"2"}
+          level="2"
           size="medium"
         >
           Team ({teams.length})
         </Heading>
-        <TeamExport />
+        {teams.length > 0 && <TeamExport />}
       </div>
-      <CardContainer>
-        {teams.map((team: ProductTeam) => (
-          <TeamCard key={team.id} team={team} />
-        ))}
-      </CardContainer>
+      {teams.length > 0 ? (
+        <CardContainer>
+          {teams.map((team: ProductTeam) => (
+            <TeamCard key={team.id} team={team} />
+          ))}
+        </CardContainer>
+      ) : (
+        <span>Ingen team i klyngen. Klyngen knyttes til teamene via teamsiden.</span>
+      )}
     </>
   );
 }

e635381e717708cd8aec19c316cfa0b41fef3d83
Johannes <johannes.moskvil@gmail.com>
2023-04-28 14:29:26 +0200

    npm lint

--- apps/frontend-2/src/pages/admin/MailLogPage.tsx
+++ apps/frontend-2/src/pages/admin/MailLogPage.tsx
@@ -2,92 +2,116 @@ import { css } from "@emotion/css";
 import { BodyLong, Heading, Pagination, Panel, Radio, RadioGroup } from "@navikt/ds-react";
 import dayjs from "dayjs";
 import { useEffect, useState } from "react";
+
 import { getMailLog } from "../../api/adminApi";
 import { Markdown } from "../../components/Markdown";
 import { PageHeader } from "../../components/PageHeader";
-import { MailLog, PageResponse } from "../../constants";
+import type { MailLog, PageResponse } from "../../constants";
 import { Group, userHasGroup, useUser } from "../../hooks";
 
 const panelStyles = css`
-    margin-top: 1rem;
-    border-radius: 5px;
-`
+  margin-top: 1rem;
+  border-radius: 5px;
+`;
 
 export const MailLogPage = () => {
-    const user = useUser();
-    const [log, setLog] = useState<PageResponse<MailLog>>({ content: [], numberOfElements: 0, pageNumber: 0, pages: 0, pageSize: 1, totalElements: 0 })
-    const [page, setPage] = useState(1)
-    const [limit, setLimit] = useState(20)
-    const [filterOutUpdates, setFilterOutUpdates] = useState(false)
+  const user = useUser();
+  const [log, setLog] = useState<PageResponse<MailLog>>({
+    content: [],
+    numberOfElements: 0,
+    pageNumber: 0,
+    pages: 0,
+    pageSize: 1,
+    totalElements: 0,
+  });
+  const [page, setPage] = useState(1);
+  const [limit, setLimit] = useState(20);
+  const [filterOutUpdates, setFilterOutUpdates] = useState(false);
 
-    useEffect(() => {
-        (async () => {
-            let res = await getMailLog(page-1, limit, filterOutUpdates)
-            setLog(res)
-        })();
-    }, [page, limit, filterOutUpdates]);
+  useEffect(() => {
+    (async () => {
+      const res = await getMailLog(page - 1, limit, filterOutUpdates);
+      setLog(res);
+    })();
+  }, [page, limit, filterOutUpdates]);
 
-    if (!userHasGroup(user, Group.ADMIN)) {
-        return <>Fant ikke siden.</>;
-    }
+  if (!userHasGroup(user, Group.ADMIN)) {
+    return <>Fant ikke siden.</>;
+  }
 
-    return (
-        <div>
-            <div className={css`display: flex; justify-content: space-between;`}>
-                <PageHeader title="Mail Log" />
+  return (
+    <div>
+      <div
+        className={css`
+          display: flex;
+          justify-content: space-between;
+        `}
+      >
+        <PageHeader title="Mail Log" />
 
-                <RadioGroup
-                    legend="Vis oppdateringer"
-                    value={filterOutUpdates.toString()}
-                    onChange={(v) => setFilterOutUpdates(v === 'true')}
-                >
-                    <div className={css`display: flex; gap: 1.5rem; margin-left: 0.5rem;`}>
-                        <Radio value="false">Ja</Radio>
-                        <Radio value="true">Nei</Radio>
-                    </div>
-                </RadioGroup>
-            </div>
-            
-            
-            <div className={css`width: 100%; margin-top: 2rem;`}>
-                
-                {log && log.content.map((l, i) => {
-                    const rowNum = log.pageNumber * log.pageSize + i + 1
-                    let html = l.body
-                    const bodyIdx = l.body.indexOf('<body>')
-                    if (bodyIdx >= 0) {
-                        html = html.substring(l.body.indexOf('<body>') + 6)
-                        html = html.substring(0, html.lastIndexOf('</body>'))
-                    }
-                    // some odd bug in html parser didnt like newlines inside <ul>
-                    html = html.replace(/\n/g, '')
+        <RadioGroup
+          legend="Vis oppdateringer"
+          onChange={(v) => setFilterOutUpdates(v === "true")}
+          value={filterOutUpdates.toString()}
+        >
+          <div
+            className={css`
+              display: flex;
+              gap: 1.5rem;
+              margin-left: 0.5rem;
+            `}
+          >
+            <Radio value="false">Ja</Radio>
+            <Radio value="true">Nei</Radio>
+          </div>
+        </RadioGroup>
+      </div>
 
-                    return (    
-                        <div className={css`margin-bottom: 3rem;`}>
-                            <Heading size="medium">
-                                #{rowNum} Tid: {dayjs(l.time).format("DD.MM.YYYY")} Til: {l.to}
-                            </Heading>
-                            <Heading size="medium">Emne: {l.subject}</Heading>
+      <div
+        className={css`
+          width: 100%;
+          margin-top: 2rem;
+        `}
+      >
+        {log &&
+          log.content.map((l, index) => {
+            const rowNumber = log.pageNumber * log.pageSize + index + 1;
+            let html = l.body;
+            const bodyIndex = l.body.indexOf("<body>");
+            if (bodyIndex >= 0) {
+              html = html.slice(Math.max(0, l.body.indexOf("<body>") + 6));
+              html = html.slice(0, Math.max(0, html.lastIndexOf("</body>")));
+            }
+            // some odd bug in html parser didnt like newlines inside <ul>
+            html = html.replace(/\n/g, "");
 
-                            <Panel border className={panelStyles}>
-                                <Markdown source={html} escapeHtml={false} />
-                            </Panel>
+            return (
+              <div
+                className={css`
+                  margin-bottom: 3rem;
+                `}
+              >
+                <Heading size="medium">
+                  #{rowNumber} Tid: {dayjs(l.time).format("DD.MM.YYYY")} Til: {l.to}
+                </Heading>
+                <Heading size="medium">Emne: {l.subject}</Heading>
 
-                        </div>
-                    )
-                })}
+                <Panel border className={panelStyles}>
+                  <Markdown escapeHtml={false} source={html} />
+                </Panel>
+              </div>
+            );
+          })}
 
-                <div className={css`display: flex; justify-content: flex-end;`}>
-                    <Pagination
-                        page={page}
-                        onPageChange={(x) => setPage(x)}
-                        count={limit}
-                        boundaryCount={1}
-                        siblingCount={1}
-                    />
-                </div>
-                
-            </div>
+        <div
+          className={css`
+            display: flex;
+            justify-content: flex-end;
+          `}
+        >
+          <Pagination boundaryCount={1} count={limit} onPageChange={(x) => setPage(x)} page={page} siblingCount={1} />
         </div>
-    );
+      </div>
+    </div>
+  );
 };

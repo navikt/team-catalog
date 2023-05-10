f6507959c5c0474f1991f8053025260d8d64f4ea
johannes <johannes.bredholt.moskvil@nav.no>
2023-05-10 12:18:17 +0200

    use public instead of public2

--- apps/frackend/src/frontendRoute.ts
+++ apps/frackend/src/frontendRoute.ts
@@ -4,7 +4,7 @@ import path from "node:path";
 import express, { Express } from "express";
 
 const indexHtmlExists = fs.existsSync("./public/index.html");
-const publicFolderPath = indexHtmlExists ? "./public2" : "./publicLocal";
+const publicFolderPath = indexHtmlExists ? "./public" : "./publicLocal";
 
 const setupStaticRoutes = (app: Express) => {
   app.use(express.static(publicFolderPath));

60f3497dcdfd9d223408fa612d18f622d8db1c0f
johannes <johannes.bredholt.moskvil@nav.no>
2023-05-15 14:20:59 +0200

    Use custom component to make tab better in dropdown

--- apps/frontend-2/src/components/header/HeaderDropdown.tsx
+++ apps/frontend-2/src/components/header/HeaderDropdown.tsx
@@ -1,90 +1,111 @@
-import { css } from "@emotion/css";
-import { MenuHamburgerIcon } from "@navikt/aksel-icons";
-import { Button, Link } from "@navikt/ds-react";
-import { Dropdown } from "@navikt/ds-react-internal";
-import { Link as ReactRouterLink } from "react-router-dom";
+import {css} from "@emotion/css";
+import {MenuHamburgerIcon} from "@navikt/aksel-icons";
+import {Button} from "@navikt/ds-react";
+import {Dropdown} from "@navikt/ds-react-internal";
+import {Link as ReactRouterLink} from "react-router-dom";
 
-import { Group, userHasGroup, useUser } from "../../hooks/useUser";
-import { intl } from "../../util/intl/intl";
+import {Group, userHasGroup, useUser} from "../../hooks/useUser";
+import {intl} from "../../util/intl/intl";
+import {ReactNode} from "react";
 
 export const HeaderDropdown = () => {
-  const user = useUser();
-  if (!user.loggedIn) {
-    return <></>;
-  }
+    const user = useUser();
+    if (!user.loggedIn) {
+        return <></>;
+    }
 
-  return (
-    <Dropdown>
-      <Button
-        as={Dropdown.Toggle}
-        className={css`
-          border: 1px solid white;
-        `}
-        icon={<MenuHamburgerIcon aria-hidden color="white" />}
-      />
-      <Dropdown.Menu placement="bottom">
-        <NavigationItems />
-        <Dropdown.Menu.GroupedList>
-          <Dropdown.Menu.GroupedList.Heading>{user.name}</Dropdown.Menu.GroupedList.Heading>
-          <Dropdown.Menu.GroupedList.Item>
-            <ReactRouterLink to={`/resource/${user.ident}`}>Min side</ReactRouterLink>
-          </Dropdown.Menu.GroupedList.Item>
-          <Dropdown.Menu.GroupedList.Item>
-            <ReactRouterLink to={`/user/notifications`}>Mine varsler</ReactRouterLink>
-          </Dropdown.Menu.GroupedList.Item>
-        </Dropdown.Menu.GroupedList>
-        <AdminItems />
-      </Dropdown.Menu>
-    </Dropdown>
-  );
+    return (
+        <Dropdown>
+            <Button
+                as={Dropdown.Toggle}
+                className={css`
+                  border: 1px solid white;
+                `}
+                icon={<MenuHamburgerIcon aria-hidden color="white"/>}
+            />
+            <Dropdown.Menu placement="bottom">
+                <NavigationItems/>
+                <Dropdown.Menu.GroupedList>
+                    <Dropdown.Menu.GroupedList.Heading>{user.name}</Dropdown.Menu.GroupedList.Heading>
+                    <MenuListItemAsReactLocationLink to={`/resource/${user.ident}`}>Min
+                        side</MenuListItemAsReactLocationLink>
+                    <MenuListItemAsReactLocationLink to={`/user/notifications`}>Mine
+                        varsler</MenuListItemAsReactLocationLink>
+                </Dropdown.Menu.GroupedList>
+                <AdminItems/>
+            </Dropdown.Menu>
+        </Dropdown>
+    );
 };
 
 function NavigationItems() {
-  return (
-    <Dropdown.Menu.GroupedList className="mobile-navigation">
-      <Dropdown.Menu.GroupedList.Heading>Navigasjon</Dropdown.Menu.GroupedList.Heading>
-      <Dropdown.Menu.GroupedList.Item>
-        <Link as={ReactRouterLink} to="/area">
-          Områder
-        </Link>
-      </Dropdown.Menu.GroupedList.Item>
-      <Dropdown.Menu.GroupedList.Item>
-        <Link as={ReactRouterLink} to="/cluster">
-          Klynger
-        </Link>
-      </Dropdown.Menu.GroupedList.Item>
-      <Dropdown.Menu.GroupedList.Item>
-        <Link as={ReactRouterLink} to="/team">
-          Team
-        </Link>
-      </Dropdown.Menu.GroupedList.Item>
-      <Dropdown.Menu.GroupedList.Item>
-        <Link as={ReactRouterLink} to="/location/FA1">
-          Fyrstikkalléen
-        </Link>
-      </Dropdown.Menu.GroupedList.Item>
-    </Dropdown.Menu.GroupedList>
-  );
+    return (
+        <Dropdown.Menu.GroupedList className="mobile-navigation">
+            <Dropdown.Menu.GroupedList.Heading>Navigasjon</Dropdown.Menu.GroupedList.Heading>
+            <MenuListItemAsReactLocationLink to="/area">
+                Områder
+            </MenuListItemAsReactLocationLink>
+            <MenuListItemAsReactLocationLink to="/cluster">
+                Klynger
+            </MenuListItemAsReactLocationLink>
+            <MenuListItemAsReactLocationLink to="/team">
+                Team
+            </MenuListItemAsReactLocationLink>
+            <MenuListItemAsReactLocationLink to="/location/FA1">
+                Fyrstikkalléen
+            </MenuListItemAsReactLocationLink>
+        </Dropdown.Menu.GroupedList>
+    );
 }
 
 function AdminItems() {
-  const user = useUser();
-  if (!userHasGroup(user, Group.ADMIN)) {
-    return <></>;
-  }
+    const user = useUser();
+    if (!userHasGroup(user, Group.ADMIN)) {
+        return <></>;
+    }
 
-  return (
-    <Dropdown.Menu.GroupedList>
-      <Dropdown.Menu.GroupedList.Heading>Admin</Dropdown.Menu.GroupedList.Heading>
-      <Dropdown.Menu.GroupedList.Item>
-        <ReactRouterLink to="/admin/audit">{intl.audit}</ReactRouterLink>
-      </Dropdown.Menu.GroupedList.Item>
-      <Dropdown.Menu.GroupedList.Item>
-        <ReactRouterLink to="/admin/maillog">{intl.mailLog}</ReactRouterLink>
-      </Dropdown.Menu.GroupedList.Item>
-      <Dropdown.Menu.GroupedList.Item>
-        <ReactRouterLink to="/admin/settings">{intl.settings}</ReactRouterLink>
-      </Dropdown.Menu.GroupedList.Item>
-    </Dropdown.Menu.GroupedList>
-  );
+    return (
+        <Dropdown.Menu.GroupedList>
+            <Dropdown.Menu.GroupedList.Heading>Admin</Dropdown.Menu.GroupedList.Heading>
+            <MenuListItemAsReactLocationLink to="/admin/audit">{intl.audit}</MenuListItemAsReactLocationLink>
+            <MenuListItemAsReactLocationLink to="/admin/maillog">{intl.mailLog}</MenuListItemAsReactLocationLink>
+            <MenuListItemAsReactLocationLink to="/admin/settings">{intl.settings}</MenuListItemAsReactLocationLink>
+        </Dropdown.Menu.GroupedList>
+    );
+}
+
+/**
+ * Use this component to enforce that MenuItems that uses Link from React Location applies same styles as a native MenuItem.
+ */
+function MenuListItemAsReactLocationLink({children, to}: { children: ReactNode; to: string }) {
+    return (
+        <Dropdown.Menu.List.Item
+            as="span"
+            className={css`
+              &:focus-within {
+                outline: none;
+                box-shadow: inset 0 0 0 2px var(--a-border-focus);
+              }
+            `}
+        >
+            <ReactRouterLink
+                className={css`
+                  width: 100%;
+
+                  &:focus-visible {
+                    outline: none;
+                    box-shadow: none;
+                  }
+
+                  &:active {
+                    background-color: var(--ac-dropdown-item-active-bg, var(--a-surface-action-active));
+                    color: var(--ac-dropdown-item-active-text, var(--a-text-on-action));
+                  }
+                `}
+                to={to}
+            >
+                {children}
+            </ReactRouterLink>
+        </Dropdown.Menu.List.Item>
+    );
 }

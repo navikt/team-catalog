de31784ac826249a81d211793b687b6284e01f2f
Johannes <johannes.moskvil@gmail.com>
2023-05-16 11:01:41 +0200

    Refactor LocationPage

--- apps/frontend-2/src/pages/location/BuildingInfo.tsx
+++ apps/frontend-2/src/pages/location/BuildingInfo.tsx
@@ -1,13 +1,8 @@
-import { css } from "@emotion/css";
-import { Heading } from "@navikt/ds-react";
-
-import locationRessources from "../../assets/locationRessources.svg";
-import locationTeams from "../../assets/locationTeams.svg";
-import { OfficeDaysChart } from "../../components/charts/OfficeDaysChart";
-import { ResourceCard } from "../../components/common/ResourceCard";
 import { LargeDivider } from "../../components/Divider";
 import type { LocationSimple } from "../../constants";
 import type { LocationSummary } from "../../hooks";
+import { LocationHeader } from "./components/LocationHeader";
+import { LocationStats } from "./components/LocationStats";
 
 type BuildingProperties = {
   locationCode: string;
@@ -16,69 +11,21 @@ type BuildingProperties = {
   sectionList: LocationSimple[];
 };
 
-const iconWithTextStyle = css`
-  display: flex;
-  gap: 0.5rem;
-  align-items: center;
-`;
-
 export const BuildingInfo = (properties: BuildingProperties) => {
   const { locationCode, sectionList, locationBuilding, locationStats } = properties;
   return (
     <>
-      <Heading level="1" size="large">
-        {locationBuilding?.displayName}
-      </Heading>
-      <p>Siden viser team som har lagt inn informasjon om lokasjon og hvilke dager de er på kontoret.</p>
-      <div
-        className={css`
-          display: flex;
-          gap: 1rem;
-          flex-wrap: wrap;
-        `}
-      >
-        <div className={iconWithTextStyle}>
-          <img alt={""} src={locationTeams} width="50px" />
-          {locationStats[locationCode].teamCount} teams
-        </div>
-        <div className={iconWithTextStyle}>
-          <img alt={""} src={locationRessources} width="50px" />
-          {locationStats[locationCode].resourceCount} personer
-        </div>
-      </div>
+      <LocationHeader
+        displayName={locationBuilding?.displayName}
+        resourceCount={locationStats[locationCode].resourceCount}
+        teamCount={locationStats[locationCode].teamCount}
+      />
       <LargeDivider />
-      <div
-        className={css`
-          display: flex;
-          flex-wrap: wrap;
-          gap: 2rem;
-        `}
-      >
-        <div>
-          <Heading level="2" size="medium" spacing>
-            Slik er vi fordelt i {locationBuilding.description}
-          </Heading>
-          <div
-            className={css`
-              display: flex;
-              flex-direction: column;
-              gap: 1rem;
-            `}
-          >
-            {sectionList.map((section) => (
-              <ResourceCard
-                color={"#E6F1F8"}
-                key={section.code}
-                name={section.displayName}
-                numberOfMembers={locationStats[section.code]?.resourceCount}
-                numberOfTeams={locationStats[section.code]?.teamCount}
-                url={`/location/${section.code}`}
-              />
-            ))}
-          </div>
-        </div>
-        <OfficeDaysChart />
-      </div>
+      <LocationStats
+        description={locationBuilding.description}
+        locationStats={locationStats}
+        simpleLocationList={sectionList}
+      />
     </>
   );
 };

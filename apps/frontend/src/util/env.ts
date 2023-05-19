b1e176c927012bbb79c0b51cd475bd0c49f75677
Johannes <johannes.moskvil@gmail.com>
2023-05-19 09:51:44 +0200

    Delete old frontend dir

--- apps/frontend/src/util/env.ts
+++
@@ -1,9 +0,0 @@
-export const env = {
-  teamCatalogBaseUrl: process.env.REACT_APP_TEAMCATALOG_ENDPOINT,
-  nomApiBaseUrl: process.env.REACT_APP_NOMAPI_ENDPOINT,
-  amplitudeEndpoint: process.env.REACT_APP_AMPLITUDE_ENDPOINT,
-  amplitudeApiKey: process.env.REACT_APP_AMPLITUDE_API_KEY,
-  githubVersion: process.env.REACT_APP_GIT_VERSION || 'local',
-  processCatBaseUrl: process.env.REACT_APP_PROCESS_CAT_BASE_URL,
-  isSandbox: window.location.host.indexOf('sandbox') >= 0,
-}

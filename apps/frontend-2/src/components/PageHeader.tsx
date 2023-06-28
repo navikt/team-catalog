06a9689ee8f1c5743a321d7410cf9c94ffef8c27
johannes <johannes.bredholt.moskvil@nav.no>
2023-06-28 12:19:42 +0200

    Refactor to Tag and make status display to the right

--- apps/frontend-2/src/components/PageHeader.tsx
+++ apps/frontend-2/src/components/PageHeader.tsx
@@ -1,9 +1,10 @@
 import { css } from "@emotion/css";
-import { Heading } from "@navikt/ds-react";
+import type { TagProps } from "@navikt/ds-react";
+import { Heading, Tag } from "@navikt/ds-react";
 import type { ReactNode } from "react";
 
-import type { Status } from "../constants";
-import { StatusField } from "./StatusField";
+import { Status } from "../constants";
+import { intl } from "../util/intl/intl";
 
 export function PageHeader({ title, status, children }: { title: string; status?: Status; children?: ReactNode }) {
   return (
@@ -15,28 +16,57 @@ export function PageHeader({ title, status, children }: { title: string; status?
         flex-wrap: wrap;
         margin-bottom: 1.5rem;
 
-        > :nth-last-child(2) {
-          flex: 1;
-        }
-
         h1 {
           white-space: nowrap;
         }
       `}
     >
-      {status && <StatusField status={status} />}
-      <Heading level="1" size="large">
-        {title}
-      </Heading>
+      <span
+        className={css`
+          display: flex;
+          gap: 1rem;
+          flex: 1;
+          align-items: center;
+        `}
+      >
+        <Heading level="1" size="large">
+          {title}
+        </Heading>
+        <StatusTag status={status} />
+      </span>
       <div
         className={css`
           display: flex;
           gap: 1rem;
-          flex-wrap: wrap;
         `}
       >
         {children}
       </div>
     </div>
+  );
+}
+
+function StatusTag({ status }: { status?: Status }) {
+  if (!status) {
+    return;
+  }
+
+  let variant: TagProps["variant"] = "success";
+  if (status === Status.INACTIVE) {
+    variant = "error";
+  }
+  if (status === Status.PLANNED) {
+    variant = "info";
+  }
+
+  return (
+    <Tag
+      className={css`
+        width: max-content;
+      `}
+      variant={variant}
+    >
+      {intl[status]}
+    </Tag>
   );
 }

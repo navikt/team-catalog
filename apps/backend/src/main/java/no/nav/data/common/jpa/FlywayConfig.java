cccf0110d86402106ab179c08a2b74246bf72059
Eziz Annagurban <eziztm@yahoo.com>
2023-09-01 09:47:05 +0200

    first

--- apps/backend/src/main/java/no/nav/data/common/jpa/FlywayConfig.java
+++ apps/backend/src/main/java/no/nav/data/common/jpa/FlywayConfig.java
@@ -18,32 +18,32 @@ import org.springframework.context.annotation.Configuration;
 @ConditionalOnProperty(value = "vault.enabled", matchIfMissing = true)
 public class FlywayConfig {
 
-    @Bean
-    public FlywayConfigurationCustomizer flywayConfigurationCustomizer(VaultConfig vaultConfig, DataSourceProperties properties) {
-        return configuration -> {
-            String adminRole = vaultConfig.getDatabaseAdminrole();
-            String path = vaultConfig.getDatabaseBackend() + "/creds/" + adminRole;
-            log.info("Getting credentials for role {}", adminRole);
-            LogicalResponse response = read(path);
-            String username = response.getData().get("username");
-            String password = response.getData().get("password");
-            log.info("Setting datasource for flyway with user {} and role {}", username, adminRole);
-
-            HikariConfig config = DatasourceConfig.createHikariConfig(properties);
-            config.setUsername(username);
-            config.setPassword(password);
-
-            configuration
-                    .dataSource(new HikariDataSource(config))
-                    .initSql(String.format("SET ROLE \"%s\"", adminRole));
-        };
-    }
-
-    private LogicalResponse read(String path) {
-        try {
-            return VaultUtil.getInstance().getClient().logical().read(path);
-        } catch (Exception e) {
-            throw new TechnicalException("vault error", e);
-        }
-    }
+//    @Bean
+//    public FlywayConfigurationCustomizer flywayConfigurationCustomizer(VaultConfig vaultConfig, DataSourceProperties properties) {
+//        return configuration -> {
+//            String adminRole = vaultConfig.getDatabaseAdminrole();
+//            String path = vaultConfig.getDatabaseBackend() + "/creds/" + adminRole;
+//            log.info("Getting credentials for role {}", adminRole);
+//            LogicalResponse response = read(path);
+//            String username = response.getData().get("username");
+//            String password = response.getData().get("password");
+//            log.info("Setting datasource for flyway with user {} and role {}", username, adminRole);
+//
+//            HikariConfig config = DatasourceConfig.createHikariConfig(properties);
+//            config.setUsername(username);
+//            config.setPassword(password);
+//
+//            configuration
+//                    .dataSource(new HikariDataSource(config))
+//                    .initSql(String.format("SET ROLE \"%s\"", adminRole));
+//        };
+//    }
+//
+//    private LogicalResponse read(String path) {
+//        try {
+//            return VaultUtil.getInstance().getClient().logical().read(path);
+//        } catch (Exception e) {
+//            throw new TechnicalException("vault error", e);
+//        }
+//    }
 }

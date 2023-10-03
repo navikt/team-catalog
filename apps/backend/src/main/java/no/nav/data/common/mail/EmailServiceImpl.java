ca6f0acae1f68915415e8df8c843ce2d945e5aff
Eziz Annagurban <eziztm@yahoo.com>
2023-10-03 13:13:28 +0200

    ORG-1582: enabled email

--- apps/backend/src/main/java/no/nav/data/common/mail/EmailServiceImpl.java
+++ apps/backend/src/main/java/no/nav/data/common/mail/EmailServiceImpl.java
@@ -3,11 +3,15 @@ package no.nav.data.common.mail;
 import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
 import no.nav.data.common.security.SecurityProperties;
 import no.nav.data.common.storage.StorageService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmailServiceImpl implements EmailService {
+
+    public static final Logger logger = LogManager.getLogger(EmailServiceImpl.class);
 
     private final StorageService storage;
     private final EmailProvider emailProvider;
@@ -27,6 +31,8 @@ public class EmailServiceImpl implements EmailService {
     public void sendMail(MailTask mailTask) {
         var toSend = securityProperties.isDev() ? mailTask.withSubject("[DEV] " + mailTask.getSubject()) : mailTask;
 
+        logger.info("Sending email to " + mailTask.getTo());
+
         emailClient.sendEmail(toSend);
     }
 
@@ -38,6 +44,8 @@ public class EmailServiceImpl implements EmailService {
     @SchedulerLock(name = "sendMail")
     @Scheduled(initialDelayString = "PT2M", fixedRateString = "PT1M")
     public void sendMail() {
+        logger.info("Scheduled email sending to start");
+
         var tasks = storage.getAll(MailTask.class);
 
         tasks.forEach(task -> {

aad48f7553a3229d20ae441f27d32ea232e353c0
johannbm <johannes.bredholt.moskvil@nav.no>
2023-10-10 13:56:48 +0200

    testingfor now

--- apps/frontend/src/pages/admin/AuditDiffPage.tsx
+++ apps/frontend/src/pages/admin/AuditDiffPage.tsx
@@ -1,3 +1,6 @@
+import { css } from "@emotion/css";
+import { Button } from "@navikt/ds-react";
+import { useState } from "react";
 import ReactJsonViewCompare from "react-json-view-compare";
 import { useQuery } from "react-query";
 import { useParams } from "react-router-dom";
@@ -6,17 +9,31 @@ import { auditLogKeys, getAuditLog } from "../../api/adminApi";
 
 export function AuditDiffPage() {
   const auditId = useParams<{ auditId: string }>().auditId as string;
+  const [index, setIndex] = useState(0);
 
   const auditLogQuery = useQuery({
     queryKey: auditLogKeys.id(auditId),
     queryFn: () => getAuditLog(auditId),
   });
 
-  const audit0 = auditLogQuery.data?.audits[0]?.data?.data;
-  const audit1 = auditLogQuery.data?.audits[1]?.data?.data;
+  const audit0 = auditLogQuery.data?.audits[index]?.data?.data ?? {};
+  const audit1 = auditLogQuery.data?.audits[index + 1]?.data?.data ?? {};
 
   return (
-    <div>
+    <div
+      className={css`
+        //.c-line-none {
+        //  display: none; /* Hide all elements with class A by default */
+        //}
+        //
+        //.c-line-add + .c-line-none, /* Select class A that follows a sibling with class B */
+        //.c-line-del + .c-line-none /* Select class A that follows a sibling with class C */ {
+        //  display: block; /* Display class A when it follows a sibling with class B or C */
+        //}
+      `}
+    >
+      <Button onClick={() => setIndex(index - 1)}>Tilbake</Button>
+      <Button onClick={() => setIndex(index + 1)}>Frem</Button>
       <ReactJsonViewCompare newData={audit0} oldData={audit1} />
     </div>
   );

852805e66e8ff837d81ff0dc925c05dd274dc6d9
johannbm <johannes.bredholt.moskvil@nav.no>
2023-10-18 13:29:27 +0200

    add header to version table

--- apps/frontend/src/pages/admin/AuditDiffPage.tsx
+++ apps/frontend/src/pages/admin/AuditDiffPage.tsx
@@ -1,5 +1,5 @@
 import { css } from "@emotion/css";
-import { Checkbox, Table } from "@navikt/ds-react";
+import { Checkbox, Heading, Table } from "@navikt/ds-react";
 import { useQuery } from "@tanstack/react-query";
 import dayjs from "dayjs";
 import sortBy from "lodash/sortBy";
@@ -22,73 +22,82 @@ export function AuditDiffPage() {
     queryFn: () => getAuditLog(auditId),
   });
 
+  const firstAudit = auditLogQuery.data?.audits[0];
+
   const matchingAudits = auditLogQuery.data?.audits.filter((audit) => checkedRows.includes(audit.id)) ?? [];
   const [oldestAudit, newestAudit] = sortBy(matchingAudits, "time");
 
   return (
-    <div
-      className={css`
-        display: flex;
-        gap: 1rem;
-        height: 700px;
-
-        > div {
-          overflow-y: scroll;
+    <>
+      <Heading size="medium" spacing>
+        {/* eslint-disable-next-line @typescript-eslint/ban-ts-comment */}
+        {/*@ts-ignore*/}
+        Historikk: {firstAudit?.data.type} - {firstAudit?.data.data.name}
+      </Heading>
+      <div
+        className={css`
+          display: flex;
+          gap: 1rem;
           height: 700px;
-          width: 50%;
-        }
-      `}
-    >
-      <div>
-        <Table>
-          <Table.Header>
-            <Table.Row>
-              <Table.HeaderCell scope="col">Velg</Table.HeaderCell>
-              <Table.HeaderCell scope="col">Tidspunkt</Table.HeaderCell>
-              <Table.HeaderCell scope="col">Bruker</Table.HeaderCell>
-              <Table.HeaderCell scope="col">Handling</Table.HeaderCell>
-            </Table.Row>
-          </Table.Header>
-          <Table.Body>
-            {(auditLogQuery.data?.audits ?? []).map((audit) => (
-              <Table.Row key={audit.id}>
-                <Table.DataCell>
-                  <Checkbox
-                    checked={checkedRows.includes(audit.id)}
-                    disabled={checkedRows.length >= 2 && !checkedRows.includes(audit.id)}
-                    hideLabel
-                    onChange={() =>
-                      setCheckedRows((checkedRows) => {
-                        const rowIsAlreadyChecked = checkedRows.includes(audit.id);
-                        if (rowIsAlreadyChecked) {
-                          return checkedRows.filter((row) => row !== audit.id);
-                        }
-                        return [audit.id, ...checkedRows];
-                      })
-                    }
-                  >
-                    Velg
-                  </Checkbox>
-                </Table.DataCell>
-                <Table.DataCell>{dayjs(audit.time).format("DD.MM.YYYY HH:mm")}</Table.DataCell>
-                <Table.DataCell>{audit.user}</Table.DataCell>
-                <Table.DataCell>{audit.action}</Table.DataCell>
-              </Table.Row>
-            ))}
-          </Table.Body>
-        </Table>
-      </div>
-      {checkedRows.length > 0 ? (
+
+          > div {
+            overflow-y: scroll;
+            height: 700px;
+            width: 50%;
+          }
+        `}
+      >
         <div>
-          <JsonViewTitle newestAudit={newestAudit} oldestAudit={oldestAudit} />
-          <div>
-            <ReactJsonViewCompare newData={newestAudit ?? oldestAudit} oldData={oldestAudit} />
-          </div>
+          <Table>
+            <Table.Header>
+              <Table.Row>
+                <Table.HeaderCell scope="col">Velg</Table.HeaderCell>
+                <Table.HeaderCell scope="col">Tidspunkt</Table.HeaderCell>
+                <Table.HeaderCell scope="col">Bruker</Table.HeaderCell>
+                <Table.HeaderCell scope="col">Handling</Table.HeaderCell>
+              </Table.Row>
+            </Table.Header>
+            <Table.Body>
+              {(auditLogQuery.data?.audits ?? []).map((audit) => (
+                <Table.Row key={audit.id}>
+                  <Table.DataCell>
+                    <Checkbox
+                      checked={checkedRows.includes(audit.id)}
+                      disabled={checkedRows.length >= 2 && !checkedRows.includes(audit.id)}
+                      hideLabel
+                      onChange={() =>
+                        setCheckedRows((checkedRows) => {
+                          const rowIsAlreadyChecked = checkedRows.includes(audit.id);
+                          if (rowIsAlreadyChecked) {
+                            return checkedRows.filter((row) => row !== audit.id);
+                          }
+                          return [audit.id, ...checkedRows];
+                        })
+                      }
+                    >
+                      Velg
+                    </Checkbox>
+                  </Table.DataCell>
+                  <Table.DataCell>{dayjs(audit.time).format("DD.MM.YYYY HH:mm")}</Table.DataCell>
+                  <Table.DataCell>{audit.user}</Table.DataCell>
+                  <Table.DataCell>{audit.action}</Table.DataCell>
+                </Table.Row>
+              ))}
+            </Table.Body>
+          </Table>
         </div>
-      ) : (
-        <div>Velg 2 versjoner for å se diff</div>
-      )}
-    </div>
+        {checkedRows.length > 0 ? (
+          <div>
+            <JsonViewTitle newestAudit={newestAudit} oldestAudit={oldestAudit} />
+            <div>
+              <ReactJsonViewCompare newData={newestAudit ?? oldestAudit} oldData={oldestAudit} />
+            </div>
+          </div>
+        ) : (
+          <div>Velg 2 versjoner for å se diff</div>
+        )}
+      </div>
+    </>
   );
 }
 

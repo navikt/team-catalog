69b5ed3e562f2b62e23a168947dd7eab708ab312
erik-a-e <65284304+erik-a-e@users.noreply.github.com>
2024-02-27 11:43:40 +0100

    frontend: Vertical <p> separation in description

--- apps/frontend/src/components/common/DescriptionSection.tsx
+++ apps/frontend/src/components/common/DescriptionSection.tsx
@@ -1,12 +1,18 @@
+import { css } from "@emotion/css";
 import React from "react";
 
 import { Markdown } from "../Markdown";
 import { ResourceInfoContainer } from "./ResourceInfoContainer";
 
+const addSeparationBetweenParagraphsStyle = css`
+  p {
+    margin-bottom: 1rem;
+  }
+`;
 export const DescriptionSection = ({ markdownText }: { markdownText: string }) => {
   return (
     <ResourceInfoContainer title="Om oss">
-      <div>
+      <div className={addSeparationBetweenParagraphsStyle}>
         <Markdown source={markdownText} />
       </div>
     </ResourceInfoContainer>

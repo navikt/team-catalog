de77b7d00af66b6e30bcae4202147d4acbe1f5f2
johannbm <johannes.moskvil@gmail.com>
2024-04-16 13:56:22 +0200

    Revert "use old verify"
    This reverts commit a1da7368379017e17ee213a5a4e6d396e702eb63.

--- apps/frackend/src/tokenValidation.ts
+++ apps/frackend/src/tokenValidation.ts
@@ -1,22 +1,5 @@
 import { getToken, validateToken } from "@navikt/oasis";
 import { NextFunction, Request, Response } from "express";
-import { expressjwt, GetVerificationKey } from "express-jwt";
-import jwksRsa from "jwks-rsa";
-
-import config from "./config.js";
-
-export const verifyJWTToken = expressjwt({
-  secret: jwksRsa.expressJwtSecret({
-    cache: true,
-    rateLimit: true,
-    jwksRequestsPerMinute: 5,
-    jwksUri: config.azureAd.openIdJwkUris,
-  }) as GetVerificationKey,
-
-  algorithms: ["RS256"],
-  audience: config.azureAd.clientId,
-  issuer: config.azureAd.issuer,
-});
 
 export const verifyToken = async (
   request: Request,

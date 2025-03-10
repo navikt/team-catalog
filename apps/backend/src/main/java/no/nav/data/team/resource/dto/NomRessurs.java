7ffd1e4b0b64c6c4677e3e02e5c1253a4addf24c
Menua Armen <menua.armen@nav.no>
2025-03-10 10:03:35 +0100

    ORG-245: Korrigert ressyrstype mapping

--- apps/backend/src/main/java/no/nav/data/team/resource/dto/NomRessurs.java
+++ apps/backend/src/main/java/no/nav/data/team/resource/dto/NomRessurs.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import no.nav.data.team.resource.domain.ResourceType;
 import org.apache.commons.lang3.StringUtils;
 
 import java.time.LocalDate;
@@ -48,7 +49,7 @@ public class NomRessurs {
         return NomRessurs.builder()
                 .personident(ressursState.personident())
                 .navident(ressursState.navident())
-                .ressurstype(ressursState.sektor())
+                .ressurstype(ResourceType.fromRessursType(ressursState.sektor()).name())
                 .fornavn(String.join(" ", Stream.of(
                                 ressursState.visningsnavn().fornanvn(),
                                 ressursState.visningsnavn().mellomnavn()

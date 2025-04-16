710985d0eebc198b40d214fae88c2af7a7f6a265
erik-a-e <65284304+erik-a-e@users.noreply.github.com>
2025-04-16 16:30:40 +0200

    frackend: eslint fix/updates

--- apps/frackend/eslint.config.mjs
+++ apps/frackend/eslint.config.mjs
@@ -1,26 +1,21 @@
+import eslintPluginSimpleImportSort from "eslint-plugin-simple-import-sort";
+import prettierRecommended from "eslint-plugin-prettier/recommended";
+import tseslint from "typescript-eslint";
+import unicorn from "eslint-plugin-unicorn";
+
 const IGNORED_UNICORN_RULES = {
   "unicorn/filename-case": "off",
 };
 
-// eslint-disable-next-line unicorn/prefer-module,no-undef
-module.exports = {
-  parser: "@typescript-eslint/parser",
-  env: {
-    es2022: true,
-    node: true,
+export default [
+  { files: ["src/**/*.ts", "./eslint.config.mjs"], ignores: ["dist/*"] },
+  ...tseslint.configs.recommended,
+  prettierRecommended,
+  unicorn.configs.recommended,
+  {
+    plugins: {
+      eslintPluginSimpleImportSort,
+    },
   },
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier", "plugin:unicorn/recommended"],
-  overrides: [],
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-  },
-  plugins: ["react", "prettier", "@typescript-eslint", "simple-import-sort"],
-  rules: {
-    "prettier/prettier": ["error"],
-    "import/prefer-default-export": "off",
-    "simple-import-sort/imports": "error",
-    "simple-import-sort/exports": "error",
-    ...IGNORED_UNICORN_RULES,
-  },
-};
+  { rules: { ...IGNORED_UNICORN_RULES } },
+];

12ede9f420cee73d69dcbe0fae3a0e672519e3f0
Balder Riiser Haugerud <Balder.Riiser.Haugerud@nav.no>
2025-05-22 10:23:06 +0200

    ORG-2538: Legger til exception handler

--- apps/backend/src/main/java/no/nav/data/team/po/ProductAreaController.java
+++ apps/backend/src/main/java/no/nav/data/team/po/ProductAreaController.java
@@ -8,6 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import no.nav.data.common.TeamCatalogProps;
 import no.nav.data.common.exceptions.ValidationException;
 import no.nav.data.common.rest.RestResponsePage;
+import no.nav.data.common.rest.StandardResponse;
 import no.nav.data.common.utils.StreamUtils;
 import no.nav.data.team.po.domain.ProductArea;
 import no.nav.data.team.po.dto.AddTeamsToProductAreaRequest;
@@ -16,15 +17,7 @@ import no.nav.data.team.po.dto.ProductAreaResponse;
 import no.nav.data.team.shared.domain.DomainObjectStatus;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Objects;
 import java.util.UUID;
@@ -137,6 +130,15 @@ public class ProductAreaController {
 
     private ProductAreaResponse convertProductAreaToReponse(ProductArea pa){
         return pa.convertToResponse(teamCatalogProps.getDefaultProductareaUuid());
+    }
+
+    @ExceptionHandler({
+            Exception.class
+    })
+    public ResponseEntity<StandardResponse> handleException(RuntimeException e) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(StandardResponse.builder().message(e.getMessage()).build());
     }
 
 }

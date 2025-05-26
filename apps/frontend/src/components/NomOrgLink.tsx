6981b42d3851b1b187406a288c5080a8e5e833e7
Hama Keli <hama.keli@nav.no>
2025-05-26 09:34:53 +0200

    ORG-2543: Endret Link komponenten til Aksel sin og ikke react-router-dom, la til inlineText bool for å sette ikonet på linje med teksten

--- apps/frontend/src/components/NomOrgLink.tsx
+++ apps/frontend/src/components/NomOrgLink.tsx
@@ -1,16 +1,17 @@
 import { ExternalLinkIcon } from "@navikt/aksel-icons";
-import { Link } from "react-router-dom";
+import { Link } from "@navikt/ds-react";
 
 import { env } from "../util/env";
 import { linkWithUnderline } from "../util/styles";
 
-export const NomOrgLink = ({ nomId, tekst }: { nomId: string; tekst: string | undefined }) => {
+export const NomOrgLink = ({ nomId, tekst }: { nomId: string; tekst?: string }) => {
   return (
     <Link
       className={linkWithUnderline}
+      href={env.isDev ? `https://nom.ekstern.dev.nav.no/org/${nomId}` : `https://nom.nav.no/org/${nomId}`}
+      inlineText
       rel="noopener noreferrer"
       target="_blank"
-      to={env.isDev ? `https://nom.ekstern.dev.nav.no/org/${nomId}` : `https://nom.nav.no/org/${nomId}`}
     >
       {tekst ? tekst : nomId}
       <ExternalLinkIcon />

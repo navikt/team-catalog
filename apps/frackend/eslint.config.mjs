f4f24dab6a399ca62446f31139dbe15c68ebf307
erik-a-e <65284304+erik-a-e@users.noreply.github.com>
2025-05-29 16:53:45 +0200

    frackend: lint config tweak

--- apps/frackend/eslint.config.mjs
+++ apps/frackend/eslint.config.mjs
@@ -2,13 +2,15 @@ import eslintPluginSimpleImportSort from "eslint-plugin-simple-import-sort";
 import prettierRecommended from "eslint-plugin-prettier/recommended";
 import tseslint from "typescript-eslint";
 import unicorn from "eslint-plugin-unicorn";
+import { globalIgnores } from "eslint/config";
 
 const IGNORED_UNICORN_RULES = {
   "unicorn/filename-case": "off",
 };
 
 export default [
-  { files: ["src/**/*.ts", "./eslint.config.mjs"], ignores: ["dist/*"] },
+  globalIgnores(["**/dist/"]),
+  { files: ["src/**/*.ts", "./eslint.config.mjs"] },
   ...tseslint.configs.recommended,
   prettierRecommended,
   unicorn.configs.recommended,

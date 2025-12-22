5be1b842178604557acda56d6e787d4a57be51ca
erik-a-e <65284304+erik-a-e@users.noreply.github.com>
2025-12-22 14:23:25 +0100

    backend: reinit-shedlock

---
+++ apps/backend/src/main/resources/db/migration/V1.14__reinit_shedlock.sql
@@ -0,0 +1,10 @@
+drop table if exists shedlock;
+
+CREATE TABLE shedlock
+(
+    name       VARCHAR(64)  NOT NULL,
+    lock_until TIMESTAMP    NOT NULL,
+    locked_at  TIMESTAMP    NOT NULL,
+    locked_by  VARCHAR(255) NOT NULL,
+    PRIMARY KEY (name)
+);

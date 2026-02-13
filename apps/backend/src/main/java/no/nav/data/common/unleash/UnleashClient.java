768a058bf535ea7eb4b19b692fc08048ee5a0bed
erik-a-e <65284304+erik-a-e@users.noreply.github.com>
2026-02-13 19:59:43 +0100

    backend: enable local run

--- apps/backend/src/main/java/no/nav/data/common/unleash/UnleashClient.java
+++ apps/backend/src/main/java/no/nav/data/common/unleash/UnleashClient.java
@@ -1,8 +1,11 @@
 package no.nav.data.common.unleash;
 
 import io.getunleash.DefaultUnleash;
+import io.getunleash.MoreOperations;
+import io.getunleash.Unleash;
 import io.getunleash.UnleashContext;
 import io.getunleash.util.UnleashConfig;
+import io.getunleash.variant.Variant;
 import lombok.extern.slf4j.Slf4j;
 import no.nav.data.common.security.azure.AzureUserInfo;
 import org.springframework.context.annotation.Profile;
@@ -10,16 +13,44 @@ import org.springframework.core.env.Environment;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
 import java.util.Optional;
+import java.util.function.BiPredicate;
 
 @Slf4j
 @Service
 @Profile("!test")
 public class UnleashClient {
 
-    private final DefaultUnleash client;
+    private final Unleash client;
 
     public UnleashClient(Environment environment) {
+        if(Arrays.asList(environment.getActiveProfiles()).contains("local")) {
+            // dummy unleash when local
+            this.client = new Unleash(){
+
+                @Override
+                public boolean isEnabled(String s, UnleashContext unleashContext, BiPredicate<String, UnleashContext> biPredicate) {
+                    return false;
+                }
+
+                @Override
+                public Variant getVariant(String s, UnleashContext unleashContext) {
+                    return null;
+                }
+
+                @Override
+                public Variant getVariant(String s, UnleashContext unleashContext, Variant variant) {
+                    return null;
+                }
+
+                @Override
+                public MoreOperations more() {
+                    return null;
+                }
+            };
+            return;
+        }
 
         UnleashConfig config = UnleashConfig.builder()
                 .appName("team-catalog-backend")
